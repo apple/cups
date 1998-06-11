d668cc74712a4af662c4d2072ac97daa46206098
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
1998-06-11 18:35:02 +0000

    Initial revision
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@50 7a7537e8-13f0-0310-91df-b6672ffda945

---
+++ cups/mark.c
@@ -0,0 +1,51 @@
+/*
+ * "$Id: mark.c,v 1.1 1998/06/11 18:35:02 mike Exp $"
+ *
+ *   for the XYZ library.
+ *
+ *   Copyright 1997-1998 by Easy Software Products.
+ *
+ *       Easy Software Products
+ *       44145 Airport View Drive, Suite 204
+ *       Hollywood, Maryland 20636-3111 USA
+ *
+ *       Voice: (301) 373-9603
+ *       EMail: cups-info@cups.org
+ *         WWW: http://www.cups.org
+ *
+ *   This library is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU Library General Public License as published
+ *   by the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *   USA.
+ *
+ * Contents:
+ *
+ *
+ * Revision History:
+ *
+ *   $Log: mark.c,v $
+ *   Revision 1.1  1998/06/11 18:35:02  mike
+ *   Initial revision
+ *
+ */
+
+/*
+ * Include necessary headers.
+ */
+
+#include
+
+
+/*
+ * End of "$Id: mark.c,v 1.1 1998/06/11 18:35:02 mike Exp $".
+ */
