d853e109168fc0f2e4c864a4bd7d3dcbc2cffaf6
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
1998-12-07 17:43:32 +0000

    Initial revision
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@104 7a7537e8-13f0-0310-91df-b6672ffda945

---
+++ cups/cups.h
@@ -0,0 +1,37 @@
+/*
+ * "$Id: cups.h,v 1.1 1998/12/07 17:43:32 mike Exp $"
+ *
+ *   for the Common UNIX Printing System (CUPS).
+ *
+ *   Copyright 1997-1998 by Easy Software Products.
+ *
+ *   These coded instructions, statements, and computer programs are the
+ *   property of Easy Software Products and are protected by Federal
+ *   copyright law.  Distribution and use rights are outlined in the file
+ *   "LICENSE.txt" which should have been included with this file.  If this
+ *   file is missing or damaged please contact Easy Software Products
+ *   at:
+ *
+ *       Attn: CUPS Licensing Information
+ *       Easy Software Products
+ *       44145 Airport View Drive, Suite 204
+ *       Hollywood, Maryland 20636-3111 USA
+ *
+ *       Voice: (301) 373-9603
+ *       EMail: cups-info@cups.org
+ *         WWW: http://www.cups.org
+ *
+ * Contents:
+ *
+ */
+
+/*
+ * Include necessary headers...
+ */
+
+#include
+
+
+/*
+ * End of "$Id: cups.h,v 1.1 1998/12/07 17:43:32 mike Exp $".
+ */
