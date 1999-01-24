04c3c0a19857df311623a2136f8838c61940d221
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
1999-01-24 14:24:06 +0000

    Initial revision
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@110 7a7537e8-13f0-0310-91df-b6672ffda945

---
+++ cups/testhttp.c
@@ -0,0 +1,50 @@
+/*
+ * "$Id: testhttp.c,v 1.1 1999/01/24 14:18:43 mike Exp $"
+ *
+ *   HTTP test program for the Common UNIX Printing System (CUPS).
+ *
+ *   Copyright 1997-1999 by Easy Software Products.
+ *
+ *   These coded instructions, statements, and computer programs are the
+ *   property of Easy Software Products and are protected by Federal
+ *   copyright law.  Distribution and use rights are outlined in the file
+ *   "LICENSE.txt" which should have been included with this file.  If this
+ *   file is missing or damaged please contact Easy Software Products
+ *   at:
+ *
+ *       Attn: CUPS Licensing Information
+ *       Easy Software Products
+ *       44145 Airport View Drive, Suite 204
+ *       Hollywood, Maryland 20636-3111 USA
+ *
+ *       Voice: (301) 373-9603
+ *       EMail: cups-info@cups.org
+ *         WWW: http://www.cups.org
+ *
+ * Contents:
+ *
+ *   main() - Main entry.
+ */
+
+/*
+ * Include necessary headers...
+ */
+
+#include <config.h>
+#include "http.h"
+
+
+/*
+ * 'main()' - Main entry.
+ */
+
+int			/* O - Exit status */
+main(int  argc,		/* I - Number of command-line arguments */
+     char *argv[])	/* I - Command-line arguments */
+{
+}
+
+
+/*
+ * End of "$Id: testhttp.c,v 1.1 1999/01/24 14:18:43 mike Exp $".
+ */
