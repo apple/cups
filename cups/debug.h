f6f95bd53ae163e9286373fe4991ace218fc9bc9
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
1999-04-21 19:43:58 +0000

    Didn't add this header...
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@241 7a7537e8-13f0-0310-91df-b6672ffda945

---
+++ cups/debug.h
@@ -0,0 +1,57 @@
+/*
+ * "$Id: debug.h,v 1.1 1999/04/21 19:43:58 mike Exp $"
+ *
+ *   Debugging macros for the Common UNIX Printing System (CUPS).
+ *
+ *   Copyright 1997-1999 by Easy Software Products.
+ *
+ *   These coded instructions, statements, and computer programs are the
+ *   property of Easy Software Products and are protected by Federal
+ *   copyright law.  Distribution and use rights are outlined in the file
+ *   "LICENSE.txt" which should have been included with this file.  If this
+ *   file is missing or damaged please contact Easy Software Products
+ *   at:
+ *
+ *       Attn: CUPS Licensing Information
+ *       Easy Software Products
+ *       44145 Airport View Drive, Suite 204
+ *       Hollywood, Maryland 20636-3111 USA
+ *
+ *       Voice: (301) 373-9603
+ *       EMail: cups-info@cups.org
+ *         WWW: http://www.cups.org
+ */
+
+#ifndef _CUPS_DEBUG_H_
+#  define _CUPS_DEBUG_H_
+
+/*
+ * Include necessary headers...
+ */
+
+#  include <stdio.h>
+
+/*
+ * The debug macros are used if you compile with DEBUG defined.
+ *
+ * Usage:
+ *
+ *   DEBUG_puts("string")
+ *   DEBUG_printf(("format string", arg, arg, ...));
+ *
+ * Note the extra parenthesis around the DEBUG_printf macro...
+ */
+
+#  ifdef DEBUG
+#    define DEBUG_puts(x) puts(x)
+#    define DEBUG_printf(x) printf x
+#  else
+#    define DEBUG_puts(x)
+#    define DEBUG_printf(x)
+#  endif /* DEBUG */
+
+#endif /* !_CUPS_DEBUG_H_ */
+
+/*
+ * End of "$Id: debug.h,v 1.1 1999/04/21 19:43:58 mike Exp $".
+ */
