b80c16b32f6f8f361923786eee4c99afacc4ada2
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
1999-08-10 17:21:05 +0000

    The image RIP wasn't setting the NumCopies field all the time.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@610 7a7537e8-13f0-0310-91df-b6672ffda945

--- filter/imagetoraster.c
+++ filter/imagetoraster.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: imagetoraster.c,v 1.25 1999/07/27 16:46:26 mike Exp $"
+ * "$Id: imagetoraster.c,v 1.26 1999/08/10 17:21:05 mike Exp $"
  *
  *   Image file to raster filter for the Common UNIX Printing System (CUPS).
  *
@@ -554,6 +554,8 @@ main(int  argc,		/* I - Number of command-line arguments */
 
     Copies = 1;
   }
+  else
+    header.NumCopies = 1;
 
  /*
   * Create the dithering lookup tables...
@@ -3836,5 +3838,5 @@ make_lut(ib_t  *lut,		/* I - Lookup table */
 
 
 /*
- * End of "$Id: imagetoraster.c,v 1.25 1999/07/27 16:46:26 mike Exp $".
+ * End of "$Id: imagetoraster.c,v 1.26 1999/08/10 17:21:05 mike Exp $".
  */
