7d7af39c66300a2d8a50de077d6a8f16169f2dcb
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2001-04-02 19:53:14 +0000

    Start of notification support.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.2@1666 7a7537e8-13f0-0310-91df-b6672ffda945

---
+++ notifier/mailto.c
@@ -0,0 +1,50 @@
+/*
+ * "$Id: mailto.c,v 1.1.2.1 2001/04/02 19:53:14 mike Exp $"
+ *
+ *   "mailto" notifier for the Common UNIX Printing System (CUPS).
+ *
+ *   Copyright 1997-2001 by Easy Software Products.
+ *
+ *   These coded instructions, statements, and computer programs are the
+ *   property of Easy Software Products and are protected by Federal
+ *   copyright law.  Distribution and use rights are outlined in the file
+ *   "LICENSE.txt" which should have been included with this file.  If this
+ *   file is missing or damaged please contact Easy Software Products
+ *   at:
+ *
+ *       Attn: CUPS Licensing Information
+ *       Easy Software Products
+ *       44141 Airport View Drive, Suite 204
+ *       Hollywood, Maryland 20636-3111 USA
+ *
+ *       Voice: (301) 373-9600
+ *       EMail: cups-info@cups.org
+ *         WWW: http://www.cups.org
+ *
+ * Contents:
+ *
+ */
+
+/*
+ * Include necessary headers...
+ */
+
+#include <cups/cups.h>
+#include <cups/language.h>
+#include <cups/string.h>
+
+
+/*
+ * 'main()' - Main entry for the mailto notifier.
+ */
+
+int				/* O - Exit status */
+main(int  argc,			/* I - Number of command-line arguments */
+     char *argv[])		/* I - Command-line arguments */
+{
+}
+
+
+/*
+ * End of "$Id: mailto.c,v 1.1.2.1 2001/04/02 19:53:14 mike Exp $".
+ */
