4b28011f9271d47bfa418f0fd5efb4157c2aadba
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2002-05-16 14:01:56 +0000

    Only 1 % in front of RBINumCopies...
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@2416 7a7537e8-13f0-0310-91df-b6672ffda945

--- filter/pstops.c
+++ filter/pstops.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: pstops.c,v 1.79 2002/05/15 20:49:34 mike Exp $"
+ * "$Id: pstops.c,v 1.80 2002/05/16 14:01:56 mike Exp $"
  *
  *   PostScript filter for the Common UNIX Printing System (CUPS).
  *
@@ -366,7 +366,7 @@ main(int  argc,			/* I - Number of command-line arguments */
   * Apple uses RBI comments for various non-PPD options...
   */
 
-  printf("%%%%RBINumCopies: %d\n", Copies);
+  printf("%%RBINumCopies: %d\n", Copies);
 
  /*
   * Figure out if we should use ESPshowpage or not...
@@ -1593,5 +1593,5 @@ start_nup(int number,		/* I - Page number */
 
 
 /*
- * End of "$Id: pstops.c,v 1.79 2002/05/15 20:49:34 mike Exp $".
+ * End of "$Id: pstops.c,v 1.80 2002/05/16 14:01:56 mike Exp $".
  */
