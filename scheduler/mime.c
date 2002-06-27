8df50bec51325185cad936d87978c72221ae1184
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2002-06-27 15:14:26 +0000

    Fix null filter bug (wouldn't load a null filter from a .convs file...)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@2530 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/mime.c
+++ scheduler/mime.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: mime.c,v 1.12 2002/05/16 13:45:02 mike Exp $"
+ * "$Id: mime.c,v 1.13 2002/06/27 15:14:26 mike Exp $"
  *
  *   MIME database file routines for the Common UNIX Printing System (CUPS).
  *
@@ -502,17 +502,20 @@ load_convs(mime_t     *mime,		/* I - MIME database */
     filter = lineptr;
 
 #ifndef WIN32
-   /*
-    * Verify that the filter exists and is executable...
-    */
+    if (strcmp(filename, "-") != 0)
+    {
+     /*
+      * Verify that the filter exists and is executable...
+      */
 
-    if (filter[0] == '/')
-      strlcpy(filterprog, filter, sizeof(filterprog));
-    else
-      snprintf(filterprog, sizeof(filterprog), "%s/%s", filterpath, filter);
+      if (filter[0] == '/')
+	strlcpy(filterprog, filter, sizeof(filterprog));
+      else
+	snprintf(filterprog, sizeof(filterprog), "%s/%s", filterpath, filter);
 
-    if (access(filterprog, X_OK))
-      continue;
+      if (access(filterprog, X_OK))
+	continue;
+    }
 #endif /* !WIN32 */
 
    /*
@@ -593,5 +596,5 @@ delete_rules(mime_magic_t *rules)	/* I - Rules to free */
 
 
 /*
- * End of "$Id: mime.c,v 1.12 2002/05/16 13:45:02 mike Exp $".
+ * End of "$Id: mime.c,v 1.13 2002/06/27 15:14:26 mike Exp $".
  */
