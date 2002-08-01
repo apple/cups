359214b86fbeaaff1f723716b9a2404ec5a093a6
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2002-08-01 00:45:14 +0000

    Set the printer state to stopped before stopping the current job, otherwise the job will be lost.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@2592 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/printers.c
+++ scheduler/printers.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: printers.c,v 1.125 2002/07/23 21:16:28 mike Exp $"
+ * "$Id: printers.c,v 1.126 2002/08/01 00:45:14 mike Exp $"
  *
  *   Printer routines for the Common UNIX Printing System (CUPS).
  *
@@ -1559,7 +1559,7 @@ StopPrinter(printer_t *p)	/* I - Printer to stop */
 {
   job_t	*job;			/* Active print job */
 
-
+  p->state = IPP_PRINTER_STOPPED;
  /*
   * See if we have a job printing on this printer...
   */
@@ -1586,8 +1586,6 @@ StopPrinter(printer_t *p)	/* I - Printer to stop */
 
     SaveJob(job->id);
   }
-
-  p->state = IPP_PRINTER_STOPPED;
 }
 
 
@@ -2079,5 +2077,5 @@ write_irix_state(printer_t *p)	/* I - Printer to update */
 
 
 /*
- * End of "$Id: printers.c,v 1.125 2002/07/23 21:16:28 mike Exp $".
+ * End of "$Id: printers.c,v 1.126 2002/08/01 00:45:14 mike Exp $".
  */
