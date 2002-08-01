fd8ea9843ee6bfb4d32c33abfe0d0d91e98e34f9
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2002-08-01 01:32:30 +0000

    Don't sort choices in an option, just sort the options and groups.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@2596 7a7537e8-13f0-0310-91df-b6672ffda945

--- cups/ppd.c
+++ cups/ppd.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: ppd.c,v 1.70 2002/05/31 19:07:18 mike Exp $"
+ * "$Id: ppd.c,v 1.71 2002/08/01 01:32:30 mike Exp $"
  *
  *   PPD file routines for the Common UNIX Printing System (CUPS).
  *
@@ -45,7 +45,6 @@
  *   compare_strings() - Compare two strings.
  *   compare_groups()  - Compare two groups.
  *   compare_options() - Compare two options.
- *   compare_choices() - Compare two choices.
  */
 
 /*
@@ -87,7 +86,6 @@
 static int		compare_strings(char *s, char *t);
 static int		compare_groups(ppd_group_t *g0, ppd_group_t *g1);
 static int		compare_options(ppd_option_t *o0, ppd_option_t *o1);
-static int		compare_choices(ppd_choice_t *c0, ppd_choice_t *c1);
 static int		ppd_read(FILE *fp, char *keyword, char *option,
 			         char *text, char **string);
 static void		ppd_decode(char *string);
@@ -1411,9 +1409,6 @@ ppdOpen(FILE *fp)		/* I - File to read from */
          j > 0;
 	 j --, option ++)
     {
-      qsort(option->choices, option->num_choices, sizeof(ppd_choice_t),
-            (int (*)(const void *, const void *))compare_choices);
-
       for (k = 0; k < option->num_choices; k ++)
         option->choices[k].option = (void *)option;
     }
@@ -1432,9 +1427,6 @@ ppdOpen(FILE *fp)		/* I - File to read from */
            k > 0;
 	   k --, option ++)
       {
-	qsort(option->choices, option->num_choices, sizeof(ppd_choice_t),
-              (int (*)(const void *, const void *))compare_choices);
-
         for (m = 0; m < option->num_choices; m ++)
           option->choices[m].option = (void *)option;
       }
@@ -1647,18 +1639,6 @@ compare_options(ppd_option_t *o0,/* I - First option */
                 ppd_option_t *o1)/* I - Second option */
 {
   return (compare_strings(o0->text, o1->text));
-}
-
-
-/*
- * 'compare_choices()' - Compare two choices.
- */
-
-static int			/* O - Result of comparison */
-compare_choices(ppd_choice_t *c0,/* I - First choice */
-                ppd_choice_t *c1)/* I - Second choice */
-{
-  return (compare_strings(c0->text, c1->text));
 }
 
 
@@ -2054,5 +2034,5 @@ ppd_fix(char *string)		/* IO - String to fix */
 
 
 /*
- * End of "$Id: ppd.c,v 1.70 2002/05/31 19:07:18 mike Exp $".
+ * End of "$Id: ppd.c,v 1.71 2002/08/01 01:32:30 mike Exp $".
  */
