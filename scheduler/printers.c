3733593de6247f66114a9f51828bdc0fa142bc1f
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2002-08-01 00:45:48 +0000

    Mirror 1.1.x change.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.2@2593 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/printers.c
+++ scheduler/printers.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: printers.c,v 1.93.2.23 2002/07/23 21:20:31 mike Exp $"
+ * "$Id: printers.c,v 1.93.2.24 2002/08/01 00:45:48 mike Exp $"
  *
  *   Printer routines for the Common UNIX Printing System (CUPS).
  *
@@ -1568,7 +1568,7 @@ StopPrinter(printer_t *p)	/* I - Printer to stop */
 {
   job_t	*job;			/* Active print job */
 
-
+  p->state = IPP_PRINTER_STOPPED;
  /*
   * See if we have a job printing on this printer...
   */
@@ -1595,8 +1595,6 @@ StopPrinter(printer_t *p)	/* I - Printer to stop */
 
     SaveJob(job->id);
   }
-
-  p->state = IPP_PRINTER_STOPPED;
 }
 
 
@@ -2088,5 +2086,5 @@ write_irix_state(printer_t *p)	/* I - Printer to update */
 
 
 /*
- * End of "$Id: printers.c,v 1.93.2.23 2002/07/23 21:20:31 mike Exp $".
+ * End of "$Id: printers.c,v 1.93.2.24 2002/08/01 00:45:48 mike Exp $".
  */
