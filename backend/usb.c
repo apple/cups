d04944172ae9a39c833ce93217490ebb424ac1dd
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2002-08-09 00:19:20 +0000

    Add USB support for Solaris x86.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@2603 7a7537e8-13f0-0310-91df-b6672ffda945

--- backend/usb.c
+++ backend/usb.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: usb.c,v 1.31 2002/08/09 00:08:29 mike Exp $"
+ * "$Id: usb.c,v 1.32 2002/08/09 00:19:20 mike Exp $"
  *
  *   USB port backend for the Common UNIX Printing System (CUPS).
  *
@@ -431,6 +431,16 @@ list_devices(void)
   }
 #elif defined(__sgi)
 #elif defined(__sun)
+  int   i;                      /* Looping var */
+  char  device[255];            /* Device filename */
+
+
+  for (i = 0; i < 8; i ++)
+  {
+    sprintf(device, "/dev/usb/printer%d", i);
+    if (!access(device, 0))
+      printf("direct usb:%s \"Unknown\" \"USB Printer #%d\"\n", device, i + 1);
+  }
 #elif defined(__hpux)
 #elif defined(__osf)
 #elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
@@ -442,16 +452,16 @@ list_devices(void)
   {
     sprintf(device, "/dev/ulpt%d", i);
     if (!access(device, 0))
-      printf("direct usb:%s \"Unknown\" \"USB Printer #%d (ulpt)\"\n", device, i + 1);
+      printf("direct usb:%s \"Unknown\" \"USB Printer #%d\"\n", device, i + 1);
 
     sprintf(device, "/dev/unlpt%d", i);
     if (!access(device, 0))
-      printf("direct usb:%s \"Unknown\" \"USB Printer #%d\" (unlpt)\n", device, i + 1);
+      printf("direct usb:%s \"Unknown\" \"USB Printer #%d\" (no reset)\n", device, i + 1);
   }
 #endif
 }
 
 
 /*
- * End of "$Id: usb.c,v 1.31 2002/08/09 00:08:29 mike Exp $".
+ * End of "$Id: usb.c,v 1.32 2002/08/09 00:19:20 mike Exp $".
  */
