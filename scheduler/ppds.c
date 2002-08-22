fd1cbc91cfb58243f72059997c4f48947a085ee9
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2002-08-22 16:52:57 +0000

    Fix spelling of Portuguese.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.2@2723 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/ppds.c
+++ scheduler/ppds.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: ppds.c,v 1.14.2.6 2002/06/27 19:43:48 mike Exp $"
+ * "$Id: ppds.c,v 1.14.2.7 2002/08/22 16:52:57 mike Exp $"
  *
  *   PPD scanning routines for the Common UNIX Printing System (CUPS).
  *
@@ -487,7 +487,7 @@ load_ppds(const char *d,		/* I - Actual directory */
     { "japanese",	"jp" },
     { "norwegian",	"no" },
     { "polish",		"pl" },
-    { "portugese",	"pt" },
+    { "portuguese",	"pt" },
     { "russian",	"ru" },
     { "slovak",		"sk" },
     { "swedish",	"sv" },
@@ -892,5 +892,5 @@ ppd_gets(buf_t *fp,		/* I - File to read from */
 
 
 /*
- * End of "$Id: ppds.c,v 1.14.2.6 2002/06/27 19:43:48 mike Exp $".
+ * End of "$Id: ppds.c,v 1.14.2.7 2002/08/22 16:52:57 mike Exp $".
  */
