66614f9cfbb3e50f57bdf1d27251ca4746d7b67d
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2002-10-03 14:47:58 +0000

    One more warning message fix...
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@2869 7a7537e8-13f0-0310-91df-b6672ffda945

--- backend/lpd.c
+++ backend/lpd.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: lpd.c,v 1.41 2002/10/03 13:48:19 mike Exp $"
+ * "$Id: lpd.c,v 1.42 2002/10/03 14:47:58 mike Exp $"
  *
  *   Line Printer Daemon backend for the Common UNIX Printing System (CUPS).
  *
@@ -524,7 +524,7 @@ lpd_queue(char *hostname,	/* I - Host to connect to */
 	if (error == ECONNREFUSED || error == EHOSTDOWN ||
             error == EHOSTUNREACH)
 	{
-	  fprintf(stderr, "WARNING: Network host \'%s\' is busy; will retry in 30 seconds...",
+	  fprintf(stderr, "WARNING: Network host \'%s\' is busy, down, or unreachable; will retry in 30 seconds...\n",
                   hostname);
 	  sleep(30);
 	}
@@ -908,5 +908,5 @@ rresvport(int *port)		/* IO - Port number to bind to */
 #endif /* !HAVE_RRESVPORT */
 
 /*
- * End of "$Id: lpd.c,v 1.41 2002/10/03 13:48:19 mike Exp $".
+ * End of "$Id: lpd.c,v 1.42 2002/10/03 14:47:58 mike Exp $".
  */
