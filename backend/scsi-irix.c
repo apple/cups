7414cf082ab883c0d5fa74f72fcc8ed760262b16
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2003-01-15 20:42:36 +0000

    Mirror 1.1.x changes.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.2@3100 7a7537e8-13f0-0310-91df-b6672ffda945

--- backend/scsi-irix.c
+++ backend/scsi-irix.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: scsi-irix.c,v 1.1.2.1 2002/05/27 14:47:11 mike Exp $"
+ * "$Id: scsi-irix.c,v 1.1.2.2 2003/01/15 20:42:36 mike Exp $"
  *
  *   IRIX SCSI printer support for the Common UNIX Printing System (CUPS).
  *
@@ -83,8 +83,7 @@ print_device(const char *resource,	/* I - SCSI device */
   int		bytes;			/* Number of bytes */
   int		try;			/* Current try */
   dsreq_t	scsi_req;		/* SCSI request */
-  char		scsi_cmd[6],		/* SCSI command data */
-		scsi_sense[32];		/* SCSI sense data */
+  char		scsi_cmd[6];		/* SCSI command data */
 #if defined(HAVE_SIGACTION) && !defined(HAVE_SIGSET)
   struct sigaction action;		/* Actions for POSIX signals */
 #endif /* HAVE_SIGACTION && !HAVE_SIGSET */
@@ -207,5 +206,5 @@ print_device(const char *resource,	/* I - SCSI device */
 
 
 /*
- * End of "$Id: scsi-irix.c,v 1.1.2.1 2002/05/27 14:47:11 mike Exp $".
+ * End of "$Id: scsi-irix.c,v 1.1.2.2 2003/01/15 20:42:36 mike Exp $".
  */
