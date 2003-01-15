dd88778b34ccbccaaab4263b3cd6a381384fb4c9
(no author) <(no author)@7a7537e8-13f0-0310-91df-b6672ffda945>
2003-01-15 04:25:58 +0000

    This commit was manufactured by cvs2svn to create branch 'branch-1.2'.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.2@3090 7a7537e8-13f0-0310-91df-b6672ffda945

---
+++ cups/http-private.h
@@ -0,0 +1,67 @@
+/*
+ * "$Id: http-private.h,v 1.1 2003/01/15 04:15:47 mike Exp $"
+ *
+ *   Private HTTP definitions for the Common UNIX Printing System (CUPS).
+ *
+ *   Copyright 1997-2003 by Easy Software Products, all rights reserved.
+ *
+ *   These coded instructions, statements, and computer programs are the
+ *   property of Easy Software Products and are protected by Federal
+ *   copyright law.  Distribution and use rights are outlined in the file
+ *   "LICENSE.txt" which should have been included with this file.  If this
+ *   file is missing or damaged please contact Easy Software Products
+ *   at:
+ *
+ *       Attn: CUPS Licensing Information
+ *       Easy Software Products
+ *       44141 Airport View Drive, Suite 204
+ *       Hollywood, Maryland 20636-3111 USA
+ *
+ *       Voice: (301) 373-9603
+ *       EMail: cups-info@cups.org
+ *         WWW: http://www.cups.org
+ *
+ *   This file is subject to the Apple OS-Developed Software exception.
+ */
+
+#ifndef _CUPS_HTTP_PRIVATE_H_
+#  define _CUPS_HTTP_PRIVATE_H_
+
+/*
+ * Include necessary headers...
+ */
+
+#  include "http.h"
+#  include "config.h"
+
+#  if defined HAVE_LIBSSL
+/*
+ * The OpenSSL library provides its own SSL/TLS context structure for its
+ * IO and protocol management...
+ */
+
+#    include <openssl/err.h>
+#    include <openssl/rand.h>
+#    include <openssl/ssl.h>
+
+typedef SSL http_tls_t;
+
+#  elif defined HAVE_GNUTLS
+/*
+ * The GNU TLS library is more of a "bare metal" SSL/TLS library...
+ */
+#    include <gnutls/gnutls.h>
+
+typedef struct
+{
+  gnutls_session	session;	/* GNU TLS session object */
+  void			*credentials;	/* GNU TLS credentials object */
+} http_tls_t;
+
+#  endif /* HAVE_LIBSSL */
+  
+#endif /* !_CUPS_HTTP_PRIVATE_H_ */
+
+/*
+ * End of "$Id: http-private.h,v 1.1 2003/01/15 04:15:47 mike Exp $".
+ */
