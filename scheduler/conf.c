39276c2f68342edd849fdcd5feb4963d5308dbc4
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2003-01-31 17:20:23 +0000

    Mirror 1.1.x change.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.2@3294 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/conf.c
+++ scheduler/conf.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: conf.c,v 1.77.2.25 2003/01/31 17:09:03 mike Exp $"
+ * "$Id: conf.c,v 1.77.2.26 2003/01/31 17:20:23 mike Exp $"
  *
  *   Configuration routines for the Common UNIX Printing System (CUPS).
  *
@@ -499,7 +499,7 @@ ReadConfiguration(void)
   */
 
   chown(ServerRoot, run_user, Group);
-  chmod(ServerRoot, 0755);
+  chmod(ServerRoot, 0775);
 
   snprintf(temp, sizeof(temp), "%s/certs", ServerRoot);
   chown(temp, run_user, Group);
@@ -2181,5 +2181,5 @@ CDSAGetServerCerts(void)
 
 
 /*
- * End of "$Id: conf.c,v 1.77.2.25 2003/01/31 17:09:03 mike Exp $".
+ * End of "$Id: conf.c,v 1.77.2.26 2003/01/31 17:20:23 mike Exp $".
  */
