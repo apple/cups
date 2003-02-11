b98799122f3332f77f22708730e1109ae9637108
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2003-02-11 19:21:00 +0000

    Mirror 1.1.x changes.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.2@3340 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/cups-polld.c
+++ scheduler/cups-polld.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: cups-polld.c,v 1.5.2.13 2003/02/11 19:19:27 mike Exp $"
+ * "$Id: cups-polld.c,v 1.5.2.14 2003/02/11 19:21:00 mike Exp $"
  *
  *   Polling daemon for the Common UNIX Printing System (CUPS).
  *
@@ -360,7 +360,6 @@ poll_server(http_t      *http,		/* I - HTTP connection */
 	snprintf(packet, sizeof(packet), "%x %x %s \"%s\" \"%s\" \"%s\"\n",
         	 type | CUPS_PRINTER_REMOTE, state, uri,
 		 location, info, make_model);
-        fputs(packet, stderr);
 
 	if (sendto(sock, packet, strlen(packet), 0,
 	           (struct sockaddr *)&addr, sizeof(addr)) <= 0)
@@ -409,5 +408,5 @@ poll_server(http_t      *http,		/* I - HTTP connection */
 
 
 /*
- * End of "$Id: cups-polld.c,v 1.5.2.13 2003/02/11 19:19:27 mike Exp $".
+ * End of "$Id: cups-polld.c,v 1.5.2.14 2003/02/11 19:21:00 mike Exp $".
  */
