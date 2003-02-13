5a0aab69514336db0c7822aa8ceab08a6a526141
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2003-02-13 03:33:33 +0000

    Mirror 1.1.x changes.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.2@3354 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/main.c
+++ scheduler/main.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: main.c,v 1.57.2.33 2003/02/12 19:32:29 mike Exp $"
+ * "$Id: main.c,v 1.57.2.34 2003/02/13 03:33:33 mike Exp $"
  *
  *   Scheduler main loop for the Common UNIX Printing System (CUPS).
  *
@@ -708,6 +708,9 @@ ClearString(char **s)			/* O - String value */
 
 /*
  * 'IgnoreChildSignals()' - Ignore SIGCHLD signals...
+ *
+ * We don't really ignore them, we set the signal handler to SIG_DFL,
+ * since some OS's rely on signals for the wait4() function to work.
  */
 
 void
@@ -718,16 +721,16 @@ IgnoreChildSignals(void)
 #endif /* HAVE_SIGACTION && !HAVE_SIGSET */
 
 #ifdef HAVE_SIGSET /* Use System V signals over POSIX to avoid bugs */
-  sigset(SIGCHLD, SIG_IGN);
+  sigset(SIGCHLD, SIG_DFL);
 #elif defined(HAVE_SIGACTION)
   memset(&action, 0, sizeof(action));
 
   sigemptyset(&action.sa_mask);
   sigaddset(&action.sa_mask, SIGCHLD);
-  action.sa_handler = SIG_IGN;
+  action.sa_handler = SIG_DFL;
   sigaction(SIGCHLD, &action, NULL);
 #else
-  signal(SIGCLD, SIG_IGN);	/* No, SIGCLD isn't a typo... */
+  signal(SIGCLD, SIG_DFL);	/* No, SIGCLD isn't a typo... */
 #endif /* HAVE_SIGSET */
 }
 
@@ -1023,5 +1026,5 @@ usage(void)
 
 
 /*
- * End of "$Id: main.c,v 1.57.2.33 2003/02/12 19:32:29 mike Exp $".
+ * End of "$Id: main.c,v 1.57.2.34 2003/02/13 03:33:33 mike Exp $".
  */
