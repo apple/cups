265fb4a4b3e1e2aa19b97d4fe5b1ac86d0289884
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2003-04-14 18:07:42 +0000

    Merge 1.1.x changes.
    Update standards directory.
    
    
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.2@3612 7a7537e8-13f0-0310-91df-b6672ffda945

--- cups/testipp.c
+++ cups/testipp.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: testipp.c,v 1.1.2.3 2003/04/08 03:48:06 mike Exp $"
+ * "$Id: testipp.c,v 1.1.2.4 2003/04/14 18:07:38 mike Exp $"
  *
  *   IPP test program for the Common UNIX Printing System (CUPS).
  *
@@ -34,7 +34,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <cups/string.h>
+#include <errno.h>
 #include "ipp.h"
+#ifdef WIN32
+#  include <io.h>
+#else
+#  include <unistd.h>
+#  include <fcntl.h>
+#endif /* WIN32 */
 
 
 /*
@@ -95,6 +103,7 @@ ipp_uchar_t	collection[] =			/* Collection buffer */
  */
 
 void	hex_dump(ipp_uchar_t *buffer, int bytes);
+void	print_attributes(ipp_t *ipp, int indent);
 int	read_cb(void *data, ipp_uchar_t *buffer, int bytes);
 int	write_cb(void *data, ipp_uchar_t *buffer, int bytes);
 
@@ -111,6 +120,8 @@ main(int  argc,			/* I - Number of command-line arguments */
   ipp_t		*request;	/* Request */
   ipp_state_t	state;		/* State */
   int		length;		/* Length of data */
+  int		fd;		/* File descriptor */
+  int		i;		/* Looping var */
 
 
   request = ippNew();
@@ -172,27 +183,72 @@ main(int  argc,			/* I - Number of command-line arguments */
 
   printf("%d bytes read.\n", rpos);
 
+  puts("Core IPP tests passed.");
+
+  for (i = 1; i < argc; i ++)
+  {
+    if ((fd = open(argv[i], O_RDONLY)) < 0)
+    {
+      printf("Unable to open \"%s\" - %s\n", argv[i], strerror(errno));
+      continue;
+    }
+
+    request = ippNew();
+    while ((state = ippReadFile(fd, request)) == IPP_ATTRIBUTE);
+
+    if (state != IPP_DATA)
+      printf("Error reading IPP message from \"%s\"!\n", argv[i]);
+    else
+    {
+      printf("\n%s:\n", argv[i]);
+      print_attributes(request, 4);
+    }
+
+    ippDelete(request);
+    close(fd);
+  }
+
   return (0);
 }
 
 
-void
-hex_dump(ipp_uchar_t *buffer,
-         int         bytes)
-{
-  int	i, j;
-  int	ch;
+/*
+ * 'hex_dump()' - Produce a hex dump of a buffer.
+ */
 
+void
+hex_dump(ipp_uchar_t *buffer,		/* I - Buffer to dump */
+         int         bytes)		/* I - Number of bytes */
+{
+  int	i, j;				/* Looping vars */
+  int	ch;				/* Current ASCII char */
+
+
+ /*
+  * Show lines of 16 bytes at a time...
+  */
 
   for (i = 0; i < bytes; i += 16)
   {
+   /*
+    * Show the offset...
+    */
+
     printf("%04x ", i);
+
+   /*
+    * Then up to 16 bytes in hex...
+    */
 
     for (j = 0; j < 16; j ++)
       if ((i + j) < bytes)
         printf(" %02x", buffer[i + j]);
       else
         printf("   ");
+
+   /*
+    * Then the ASCII representation of the bytes...
+    */
 
     putchar(' ');
     putchar(' ');
@@ -212,39 +268,224 @@ hex_dump(ipp_uchar_t *buffer,
 }
 
 
-int
-read_cb(void        *data,
-        ipp_uchar_t *buffer,
-	int         bytes)
-{
-  int	count;
+/*
+ * 'print_attributes()' - Print the attributes in a request...
+ */
 
+void
+print_attributes(ipp_t *ipp,		/* I - IPP request */
+                 int   indent)		/* I - Indentation */
+{
+  int			i;		/* Looping var */
+  ipp_tag_t		group;		/* Current group */
+  ipp_attribute_t	*attr;		/* Current attribute */
+  ipp_value_t		*val;		/* Current value */
+  static const char * const tags[] =	/* Value/group tag strings */
+			{
+			  "reserved-00",
+			  "operation-attributes-tag",
+			  "job-attributes-tag",
+			  "end-of-attributes-tag",
+			  "printer-attributes-tag",
+			  "unsupported-attributes-tag",
+			  "subscription-attributes-tag",
+			  "event-attributes-tag",
+			  "reserved-08",
+			  "reserved-09",
+			  "reserved-0A",
+			  "reserved-0B",
+			  "reserved-0C",
+			  "reserved-0D",
+			  "reserved-0E",
+			  "reserved-0F",
+			  "unsupported",
+			  "default",
+			  "unknown",
+			  "no-value",
+			  "reserved-14",
+			  "not-settable",
+			  "delete-attr",
+			  "admin-define",
+			  "reserved-18",
+			  "reserved-19",
+			  "reserved-1A",
+			  "reserved-1B",
+			  "reserved-1C",
+			  "reserved-1D",
+			  "reserved-1E",
+			  "reserved-1F",
+			  "reserved-20",
+			  "integer",
+			  "boolean",
+			  "enum",
+			  "reserved-24",
+			  "reserved-25",
+			  "reserved-26",
+			  "reserved-27",
+			  "reserved-28",
+			  "reserved-29",
+			  "reserved-2a",
+			  "reserved-2b",
+			  "reserved-2c",
+			  "reserved-2d",
+			  "reserved-2e",
+			  "reserved-2f",
+			  "octetString",
+			  "dateTime",
+			  "resolution",
+			  "rangeOfInteger",
+			  "begCollection",
+			  "textWithLanguage",
+			  "nameWithLanguage",
+			  "endCollection",
+			  "reserved-38",
+			  "reserved-39",
+			  "reserved-3a",
+			  "reserved-3b",
+			  "reserved-3c",
+			  "reserved-3d",
+			  "reserved-3e",
+			  "reserved-3f",
+			  "reserved-40",
+			  "textWithoutLanguage",
+			  "nameWithoutLanguage",
+			  "reserved-43",
+			  "keyword",
+			  "uri",
+			  "uriScheme",
+			  "charset",
+			  "naturalLanguage",
+			  "mimeMediaType",
+			  "memberName"
+			};
+
+
+  for (group = IPP_TAG_ZERO, attr = ipp->attrs; attr; attr = attr->next)
+  {
+    if (attr->group_tag == IPP_TAG_ZERO || !attr->name)
+    {
+      group = IPP_TAG_ZERO;
+      putchar('\n');
+      continue;
+    }
+
+    if (group != attr->group_tag)
+    {
+      group = attr->group_tag;
+
+      putchar('\n');
+      for (i = 2; i < indent; i ++)
+        putchar(' ');
+
+      puts(tags[group]);
+    }
+
+    for (i = 0; i < indent; i ++)
+      putchar(' ');
+
+    printf("%s (%s):", attr->name, tags[attr->value_tag]);
+
+    switch (attr->value_tag)
+    {
+      case IPP_TAG_ENUM :
+      case IPP_TAG_INTEGER :
+          for (i = 0, val = attr->values; i < attr->num_values; i ++, val ++)
+	    printf(" %d", val->integer);
+          putchar('\n');
+          break;
+
+      case IPP_TAG_BOOLEAN :
+          for (i = 0, val = attr->values; i < attr->num_values; i ++, val ++)
+	    printf(" %s", val->boolean ? "true" : "false");
+          putchar('\n');
+          break;
+
+      case IPP_TAG_STRING :
+      case IPP_TAG_TEXTLANG :
+      case IPP_TAG_NAMELANG :
+      case IPP_TAG_TEXT :
+      case IPP_TAG_NAME :
+      case IPP_TAG_KEYWORD :
+      case IPP_TAG_URI :
+      case IPP_TAG_URISCHEME :
+      case IPP_TAG_CHARSET :
+      case IPP_TAG_LANGUAGE :
+      case IPP_TAG_MIMETYPE :
+          for (i = 0, val = attr->values; i < attr->num_values; i ++, val ++)
+	    printf(" \"%s\"", val->string.text);
+          putchar('\n');
+          break;
+
+      case IPP_TAG_BEGIN_COLLECTION :
+          putchar('\n');
+
+          for (i = 0, val = attr->values; i < attr->num_values; i ++, val ++)
+	    print_attributes(val->collection, indent + 4);
+          break;
+
+      default :
+          putchar('\n');
+          break;
+    }
+
+  }
+}
+
+
+/*
+ * 'read_cb()' - Read data from a buffer.
+ */
+
+int					/* O - Number of bytes read */
+read_cb(void        *data,		/* I - Data */
+        ipp_uchar_t *buffer,		/* O - Buffer to read */
+	int         bytes)		/* I - Number of bytes to read */
+{
+  int	count;				/* Number of bytes */
+
+
+ /*
+  * Copy bytes from the data buffer to the read buffer...
+  */
 
   for (count = bytes; count > 0 && rpos < wused; count --, rpos ++)
     *buffer++ = wbuffer[rpos];
 
-  return (bytes - count);
-}
-
-
-int
-write_cb(void        *data,
-         ipp_uchar_t *buffer,
-	 int         bytes)
-{
-  int	count;
-
-  printf("write_cb(data=%p, buffer=%p, bytes=%d)\n", data, buffer, bytes);
-
-  for (count = bytes; count > 0 && wused < sizeof(wbuffer); count --, wused ++)
-    wbuffer[wused] = *buffer++;
-
-  printf("    count=%d, returning %d\n", count, bytes - count);
+ /*
+  * Return the number of bytes read...
+  */
 
   return (bytes - count);
 }
 
 
 /*
- * End of "$Id: testipp.c,v 1.1.2.3 2003/04/08 03:48:06 mike Exp $".
+ * 'write_cb()' - Write data into a buffer.
+ */
+
+int					/* O - Number of bytes written */
+write_cb(void        *data,		/* I - Data */
+         ipp_uchar_t *buffer,		/* I - Buffer to write */
+	 int         bytes)		/* I - Number of bytes to write */
+{
+  int	count;				/* Number of bytes */
+
+
+ /*
+  * Loop until all bytes are written...
+  */
+
+  for (count = bytes; count > 0 && wused < sizeof(wbuffer); count --, wused ++)
+    wbuffer[wused] = *buffer++;
+
+ /*
+  * Return the number of bytes written...
+  */
+
+  return (bytes - count);
+}
+
+
+/*
+ * End of "$Id: testipp.c,v 1.1.2.4 2003/04/14 18:07:38 mike Exp $".
  */
