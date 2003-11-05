cc8972336daca6cb15c19a88a2c92d4b42affc40
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2003-11-05 19:20:31 +0000

    Fix Dymo label printer driver bug (STR #373)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@3983 7a7537e8-13f0-0310-91df-b6672ffda945

--- filter/rastertodymo.c
+++ filter/rastertodymo.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: rastertodymo.c,v 1.10 2003/04/08 15:18:25 mike Exp $"
+ * "$Id: rastertodymo.c,v 1.11 2003/11/05 19:20:31 mike Exp $"
  *
  *   DYMO label printer filter for the Common UNIX Printing System (CUPS).
  *
@@ -133,7 +133,7 @@ StartPage(cups_page_header_t *header)	/* I - Page header */
 
   length = header->PageSize[1] * header->HWResolution[1] / 72;
 
-  printf("\033L%c%c", length, length >> 8);
+  printf("\033L%c%c", length >> 8, length);
   printf("\033D%c", header->cupsBytesPerLine);
 
   printf("\033%c", header->cupsCompression + 'c'); /* Darkness */
@@ -389,5 +389,5 @@ main(int  argc,		/* I - Number of command-line arguments */
 
 
 /*
- * End of "$Id: rastertodymo.c,v 1.10 2003/04/08 15:18:25 mike Exp $".
+ * End of "$Id: rastertodymo.c,v 1.11 2003/11/05 19:20:31 mike Exp $".
  */
