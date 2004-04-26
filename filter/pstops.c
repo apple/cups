76b7cbd6f632fb85c2c96018c24a0f06f2f90878
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2004-04-26 19:25:32 +0000

    Skip leading PJL commands, don't copy them (STR #664)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@4154 7a7537e8-13f0-0310-91df-b6672ffda945

--- filter/pstops.c
+++ filter/pstops.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: pstops.c,v 1.112 2004/02/25 20:14:52 mike Exp $"
+ * "$Id: pstops.c,v 1.113 2004/04/26 19:25:32 mike Exp $"
  *
  *   PostScript filter for the Common UNIX Printing System (CUPS).
  *
@@ -388,7 +388,7 @@ main(int  argc,			/* I - Number of command-line arguments */
   while (!strncmp(line, "\033%-12345X", 9))
   {
    /*
-    * Yup, we have leading PJL fun, so copy it until we hit the line
+    * Yup, we have leading PJL fun, so skip it until we hit the line
     * with "ENTER LANGUAGE"...
     */
 
@@ -399,8 +399,6 @@ main(int  argc,			/* I - Number of command-line arguments */
       len = sizeof(line);
       if (psgets(line, &len, fp) == NULL)
         break;
-
-      fwrite(line, 1, len, stdout);
     }
 
     len = sizeof(line);
@@ -1892,5 +1890,5 @@ start_nup(int number,			/* I - Page number */
 
 
 /*
- * End of "$Id: pstops.c,v 1.112 2004/02/25 20:14:52 mike Exp $".
+ * End of "$Id: pstops.c,v 1.113 2004/04/26 19:25:32 mike Exp $".
  */
