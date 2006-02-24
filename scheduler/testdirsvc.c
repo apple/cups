c6c1e2d4f12a4eab13a6f2c70c1aee849b242de4
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2006-02-24 19:04:47 +0000

    Add support for testing remote printer class discovery, too.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@5163 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/testdirsvc.c
+++ scheduler/testdirsvc.c
@@ -55,6 +55,8 @@ main(int  argc,				/* I - Number of command-line arguments */
   int		i,			/* Looping var */
 		printer,		/* Current printer */
 		num_printers,		/* Number of printers */
+		pclass,			/* Current printer class */
+		num_pclasses,		/* Number of printer classes */
 		server,			/* Current server */
 		num_servers,		/* Number of servers */
 		count,			/* Number of printers sent this cycle */
@@ -106,6 +108,7 @@ main(int  argc,				/* I - Number of command-line arguments */
   */
 
   num_printers = 10;
+  num_pclasses = 5;
   num_servers  = 1;
   interval     = 30;
   port         = 0;
@@ -135,6 +138,14 @@ main(int  argc,				/* I - Number of command-line arguments */
       else
         usage();
     }
+    else if (!strcmp(argv[i], "-C"))
+    {
+      i ++;
+      if (i < argc)
+        num_pclasses = atoi(argv[i]);
+      else
+        usage();
+    }
     else if (!strcmp(argv[i], "-p"))
     {
       i ++;
@@ -159,7 +170,8 @@ main(int  argc,				/* I - Number of command-line arguments */
       usage();
   }
 
-  if (num_printers <= 0 || num_servers <= 0 || interval <= 0 || port <= 0)
+  if ((num_printers <= 0 && num_pclasses <= 0) || num_servers <= 0 ||
+      interval <= 0 || port <= 0)
     usage();
 
  /*
@@ -207,7 +219,7 @@ main(int  argc,				/* I - Number of command-line arguments */
     printf("Sending %d printers from %d servers...\n", num_printers,
            num_servers);
 
-    count   = num_servers * num_printers / interval + 1;
+    count   = num_servers * (num_printers + num_pclasses) / interval + 1;
     curtime = time(NULL);
     curdate = localtime(&curtime);
     seconds = interval;
@@ -243,6 +255,38 @@ main(int  argc,				/* I - Number of command-line arguments */
       }
     }
 
+
+    for (i = 0, pclass = 0; pclass < num_pclasses; pclass ++)
+    {
+      for (server = 0; server < num_servers; server ++, i ++)
+      {
+        if (i == count)
+	{
+	  seconds --;
+	  i = 0;
+	  sleep(1);
+	  curtime = time(NULL);
+	  curdate = localtime(&curtime);
+	}
+
+        snprintf(packet, sizeof(packet),
+	         "%x %x ipp://testserver-%d/classes/class-%s-%d \"Server Room %d\" "
+		 "\"Test Class %d\" \"Acme Blazer 2000\"%s%s\n",
+                 CUPS_PRINTER_REMOTE | CUPS_PRINTER_CLASS, IPP_PRINTER_IDLE,
+		 server + 1, names[pclass % 26], pclass / 26 + 1, server + 1,
+		 pclass + 1, options ? " ipp-options=" : "",
+		 options ? options : "");
+
+        if (verbose)
+	  printf("[%02d:%02d:%02d] %s", curdate->tm_hour, curdate->tm_min,
+	         curdate->tm_sec, packet);
+
+        if (sendto(sock, packet, strlen(packet), 0,
+	           (struct sockaddr *)&addr, sizeof(addr)) < 0)
+	  perror("Unabled to send packet");
+      }
+    }
+
     if (!continuous)
       break;
 
@@ -265,7 +309,8 @@ main(int  argc,				/* I - Number of command-line arguments */
 void
 usage(void)
 {
-  puts("Usage: testdirsvc [-i interval] [-o ipp-options] [-p printers] [-s servers] [-v] port");
+  puts("Usage: testdirsvc [-i interval] [-o ipp-options] [-p printers] "
+       "[-C classes] [-s servers] [-v] port");
   exit(0);
 }
 
