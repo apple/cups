67669f6a3b7b9743fa2324d4085afb51334f0b06
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2006-06-16 10:21:49 +0000

    Update benchmark program to use pipes and more typical test data.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@5667 7a7537e8-13f0-0310-91df-b6672ffda945

--- filter/rasterbench.c
+++ filter/rasterbench.c
@@ -45,6 +45,9 @@
 #include "raster.h"
 #include <stdlib.h>
 #include <sys/time.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 
 /*
@@ -62,8 +65,10 @@
  */
 
 static double	compute_median(double *secs);
-static double	read_test(void);
-static double	write_test(int do_random);
+static double	get_time(void);
+static void	read_test(int fd);
+static int	run_read_test(void);
+static void	write_test(int fd);
 
 
 /*
@@ -71,69 +76,53 @@ static double	write_test(int do_random);
  */
 
 int					/* O - Exit status */
-main(int  argc,				/* I - Number of command-line args */
-     char *argv[])			/* I - Command-line arguments */
+main(void)
 {
   int		i;			/* Looping var */
-  double	secs,			/* Test times */
-		write_random_secs[TEST_PASSES],
-		read_random_secs[TEST_PASSES],
-		write_normal_secs[TEST_PASSES],
-		read_normal_secs[TEST_PASSES],
-		write_random,		/* Total write time for random */
-		read_random,		/* Total read time for random */
-		write_normal,		/* Total write time for normal */
-		read_normal;		/* Total read time for normal */
+  int		ras_fd,			/* File descriptor for read process */
+		status;			/* Exit status of read process */
+  double	start_secs,		/* Start time */
+		write_secs,		/* Write time */
+		read_secs,		/* Read time */
+		pass_secs[TEST_PASSES];	/* Total test times */
 
 
  /*
-  * Run the tests 10 times to get a good average...
+  * Ignore SIGPIPE...
+  */
+
+  signal(SIGPIPE, SIG_IGN);
+
+ /*
+  * Run the tests several times to get a good average...
   */
 
   printf("Test read/write speed of %d pages, %dx%d pixels...\n\n",
          TEST_PAGES, TEST_WIDTH, TEST_HEIGHT);
-  for (i = 0, read_normal = 0.0, read_random = 0.0,
-           write_normal = 0.0, write_random = 0.0;
-       i < TEST_PASSES;
-       i ++)
+  for (i = 0; i < TEST_PASSES; i ++)
   {
-    printf("PASS %2d: normal-write", i + 1);
+    printf("PASS %2d: ", i + 1);
     fflush(stdout);
-    secs = write_normal_secs[i] = write_test(0);
 
-    printf(" %.3f, read", secs);
+    ras_fd     = run_read_test();
+    start_secs = get_time();
+
+    write_test(ras_fd);
+
+    write_secs = get_time();
+    printf(" %.3f write,", write_secs - start_secs);
     fflush(stdout);
-    secs = read_normal_secs[i] = read_test();
 
-    printf(" %.3f, random-write", secs);
-    fflush(stdout);
-    secs = write_random_secs[i] = write_test(1);
+    close(ras_fd);
+    wait(&status);
 
-    printf(" %.3f, read", secs);
-    fflush(stdout);
-    secs = read_random_secs[i] = read_test();
-
-    printf(" %.3f\n", secs);
+    read_secs    = get_time();
+    pass_secs[i] = read_secs - start_secs;
+    printf(" %.3f read, %.3f total\n", read_secs - write_secs, pass_secs[i]);
   }
 
-  write_random = compute_median(write_random_secs);
-  read_random  = compute_median(read_random_secs);
-  write_normal = compute_median(write_normal_secs);
-  read_normal  = compute_median(read_normal_secs);
-
-  printf("\nNormal Write Time: %.3f seconds per document\n",
-         write_normal);
-  printf("Random Write Time: %.3f seconds per document\n",
-         write_random);
-  printf("Average Write Time: %.3f seconds per document\n",
-         (write_normal + write_random) / 2);
-
-  printf("\nNormal Read Time: %.3f seconds per document\n",
-         read_normal);
-  printf("Random Read Time: %.3f seconds per document\n",
-         read_random);
-  printf("Average Read Time: %.3f seconds per document\n",
-         (read_normal + read_random) / 2);
+  printf("\nMedian Total Time: %.3f seconds per document\n",
+         compute_median(pass_secs));
 
   return (0);
 }
@@ -172,39 +161,43 @@ compute_median(double *secs)		/* I - Array of time samples */
 
 
 /*
+ * 'get_time()' - Get the current time in seconds.
+ */
+
+static double				/* O - Time in seconds */
+get_time(void)
+{
+  struct timeval	curtime;	/* Current time */
+
+
+  gettimeofday(&curtime, NULL);
+  return (curtime.tv_sec + 0.000001 * curtime.tv_usec);
+}
+
+
+/*
  * 'read_test()' - Benchmark the raster read functions.
  */
 
-static double				/* O - Total time in seconds */
-read_test(void)
+static void
+read_test(int fd)			/* I - File descriptor to read from */
 {
   int			y;		/* Looping var */
-  FILE			*fp;		/* Raster file */
   cups_raster_t		*r;		/* Raster stream */
   cups_page_header_t	header;		/* Page header */
   unsigned char		buffer[8 * TEST_WIDTH];
 					/* Read buffer */
-  struct timeval	start, end;	/* Start and end times */
 
 
  /*
   * Test read speed...
   */
 
-  if ((fp = fopen("test.raster", "rb")) == NULL)
-  {
-    perror("Unable to open test.raster");
-    return (0.0);
-  }
-
-  if ((r = cupsRasterOpen(fileno(fp), CUPS_RASTER_READ)) == NULL)
+  if ((r = cupsRasterOpen(fd, CUPS_RASTER_READ)) == NULL)
   {
     perror("Unable to create raster input stream");
-    fclose(fp);
-    return (0.0);
+    return;
   }
-
-  gettimeofday(&start, NULL);
 
   while (cupsRasterReadHeader(r, &header))
   {
@@ -213,13 +206,53 @@ read_test(void)
   }
 
   cupsRasterClose(r);
+}
 
-  gettimeofday(&end, NULL);
 
-  fclose(fp);
+/*
+ * 'run_read_test()' - Run the read test as a child process via pipes.
+ */
 
-  return (end.tv_sec - start.tv_sec +
-          0.000001 * (end.tv_usec - start.tv_usec));
+static int				/* O - Standard input of child */
+run_read_test(void)
+{
+  int	ras_pipes[2];			/* Raster data pipes */
+  int	pid;				/* Child process ID */
+
+
+  if (pipe(ras_pipes))
+    return (-1);
+
+  if ((pid = fork()) < 0)
+  {
+   /*
+    * Fork error - return -1 on error...
+    */
+
+    close(ras_pipes[0]);
+    close(ras_pipes[1]);
+
+    return (-1);
+  }
+  else if (pid == 0)
+  {
+   /*
+    * Child comes here - read data from the input pipe...
+    */
+
+    close(ras_pipes[1]);
+    read_test(ras_pipes[0]);
+    exit(0);
+  }
+  else
+  {
+   /*
+    * Parent comes here - return the output pipe...
+    */
+
+    close(ras_pipes[0]);
+    return (ras_pipes[1]);
+  }
 }
 
 
@@ -227,43 +260,42 @@ read_test(void)
  * 'write_test()' - Benchmark the raster write functions.
  */
 
-static double				/* O - Total time in seconds */
-write_test(int do_random)		/* I - Do random data? */
+static void
+write_test(int fd)			/* I - File descriptor to write to */
 {
   int			page, x, y;	/* Looping vars */
-  FILE			*fp;		/* Raster file */
+  int			count;		/* Number of bytes to set */
   cups_raster_t		*r;		/* Raster stream */
   cups_page_header_t	header;		/* Page header */
-  unsigned char		data[2][8 * TEST_WIDTH];
+  unsigned char		data[32][8 * TEST_WIDTH];
 					/* Raster data to write */
-  struct timeval	start, end;	/* Start and end times */
 
 
-  if (do_random)
+ /*
+  * Create a combination of random data and repeated data to simulate
+  * text with some whitespace.
+  */
+
+  srand(time(NULL));
+
+  memset(data, 0, sizeof(data));
+
+  for (y = 0; y < 28; y ++)
   {
-   /*
-    * Create some random data to test worst-case performance...
-    */
-
-    srand(time(NULL));
-
-    for (x = 0; x < sizeof(data[0]); x ++)
+    for (x = rand() & 127, count = (rand() & 15) + 1;
+         x < sizeof(data[0]);
+         x ++, count --)
     {
-      data[0][x] = rand();
-      data[1][x] = rand();
-    }
-  }
-  else
-  {
-   /*
-    * Otherwise, create some non-random data to test the best-case
-    * performance...
-    */
+      if (count <= 0)
+      {
+	x     += (rand() & 15) + 1;
+	count = (rand() & 15) + 1;
 
-    for (x = 0; x < sizeof(data[0]); x ++)
-    {
-      data[0][x] = x & 255;		/* Gradients */
-      data[1][x] = (x & 128) ? 255 : 0;	/* Alternating */
+        if (x >= sizeof(data[0]))
+	  break;
+      }
+
+      data[y][x] = rand();
     }
   }
 
@@ -271,20 +303,11 @@ write_test(int do_random)		/* I - Do random data? */
   * Test write speed...
   */
 
-  if ((fp = fopen("test.raster", "wb")) == NULL)
-  {
-    perror("Unable to create test.raster");
-    return (0.0);
-  }
-
-  if ((r = cupsRasterOpen(fileno(fp), CUPS_RASTER_WRITE)) == NULL)
+  if ((r = cupsRasterOpen(fd, CUPS_RASTER_WRITE)) == NULL)
   {
     perror("Unable to create raster output stream");
-    fclose(fp);
-    return (0.0);
+    return;
   }
-
-  gettimeofday(&start, NULL);
 
   for (page = 0; page < TEST_PAGES; page ++)
   {
@@ -320,24 +343,10 @@ write_test(int do_random)		/* I - Do random data? */
     cupsRasterWriteHeader(r, &header);
 
     for (y = 0; y < TEST_HEIGHT; y ++)
-    {
-      if (do_random)
-        cupsRasterWritePixels(r, data[y & 1], header.cupsBytesPerLine);
-      else if (y & 128)
-        cupsRasterWritePixels(r, data[1], header.cupsBytesPerLine);
-      else
-        cupsRasterWritePixels(r, data[0], header.cupsBytesPerLine);
-    }
+      cupsRasterWritePixels(r, data[y & 31], header.cupsBytesPerLine);
   }
 
   cupsRasterClose(r);
-
-  gettimeofday(&end, NULL);
-
-  fclose(fp);
-
-  return (end.tv_sec - start.tv_sec +
-          0.000001 * (end.tv_usec - start.tv_usec));
 }
 
 
