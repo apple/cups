53dc87fd7604422e60ad26b4af5cde973bca1d2f
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2006-10-20 15:02:45 +0000

    The SNMP backend no longer uses IPP for Epson printers (STR #2028)
    backend/snmp.c:
        - probe_device(): Add Epson to the list of vendors that doesn't like
          IPP.
    
    
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@6048 7a7537e8-13f0-0310-91df-b6672ffda945

--- backend/snmp.c
+++ backend/snmp.c
@@ -1728,13 +1728,14 @@ probe_device(snmp_cache_t *device)	/* I - Device */
   debug_printf("DEBUG: %.3f Probing %s...\n", run_time(), device->addrname);
 
   if (device->make_and_model &&
-      (!strncasecmp(device->make_and_model, "Kyocera", 7) ||
+      (!strncasecmp(device->make_and_model, "Epson", 5) ||
+       !strncasecmp(device->make_and_model, "Kyocera", 7) ||
        !strncasecmp(device->make_and_model, "Lexmark", 7) ||
        !strncasecmp(device->make_and_model, "Tektronix", 9) ||
        !strncasecmp(device->make_and_model, "Xerox", 5)))
   {
    /*
-    * Kyocera, Lexmark, Tektronix, and Xerox printers often lock up on
+    * Epson, Kyocera, Lexmark, Tektronix, and Xerox printers often lock up on
     * IPP probes, so exclude them from the IPP connection test...
     */
 
