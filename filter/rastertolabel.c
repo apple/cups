0de9f758105351f6df16ea20f74f221b867663ac
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2007-02-05 21:03:49 +0000

    Copyright notice update.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@6235 7a7537e8-13f0-0310-91df-b6672ffda945

--- filter/rastertolabel.c
+++ filter/rastertolabel.c
@@ -3,7 +3,7 @@
  *
  *   Label printer filter for the Common UNIX Printing System (CUPS).
  *
- *   Copyright 2001-2006 by Easy Software Products.
+ *   Copyright 2001-2007 by Easy Software Products.
  *
  *   These coded instructions, statements, and computer programs are the
  *   property of Easy Software Products and are protected by Federal
