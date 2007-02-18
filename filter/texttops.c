957e01ab1e62c967a71a9051d8ab55d8d63c2749
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2007-02-18 13:28:41 +0000

    Fix prettyprint crash bug (STR #2158)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@6286 7a7537e8-13f0-0310-91df-b6672ffda945

--- filter/texttops.c
+++ filter/texttops.c
@@ -3,7 +3,7 @@
  *
  *   Text to PostScript filter for the Common UNIX Printing System (CUPS).
  *
- *   Copyright 1993-2005 by Easy Software Products.
+ *   Copyright 1993-2007 by Easy Software Products.
  *
  *   These coded instructions, statements, and computer programs are the
  *   property of Easy Software Products and are protected by Federal
@@ -812,7 +812,7 @@ WriteProlog(const char *title,		/* I - Title of job */
 
     puts("% Reencode base fonts");
 
-    for (i = 1 + PrettyPrint; i >= 0; i --)
+    for (i = PrettyPrint ? 2 : 1; i >= 0; i --)
       for (j = 0; j < NumFonts; j ++)
       {
 	printf("/%s findfont\n", Fonts[j][i]);
