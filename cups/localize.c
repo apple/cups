23e16df1040f25ca43743d55f2f7e4bfb524e827
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2007-03-20 01:34:29 +0000

    ppdLocalize() now supports localizing for Japanese using the "jp" locale name used by the ppdmerge program from the CUPS DDK 1.1.0 (STR #2301)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@6367 7a7537e8-13f0-0310-91df-b6672ffda945

--- cups/localize.c
+++ cups/localize.c
@@ -3,7 +3,7 @@
  *
  *   PPD custom option routines for the Common UNIX Printing System (CUPS).
  *
- *   Copyright 1997-2006 by Easy Software Products, all rights reserved.
+ *   Copyright 1997-2007 by Easy Software Products, all rights reserved.
  *
  *   These coded instructions, statements, and computer programs are the
  *   property of Easy Software Products and are protected by Federal
@@ -183,6 +183,19 @@ ppd_text(ppd_file_t *ppd,		/* I - PPD file */
   {
     snprintf(lkeyword, sizeof(lkeyword), "%s.%s", ll, keyword);
     attr = ppdFindAttr(ppd, lkeyword, spec);
+
+    if (!attr && !strcmp(ll, "ja"))
+    {
+     /*
+      * Due to a bug in the CUPS DDK 1.1.0 ppdmerge program, Japanese
+      * PPD files were incorrectly assigned "jp" as the locale name
+      * instead of "ja".  Support both the old (incorrect) and new
+      * locale names for Japanese...
+      */
+
+      snprintf(lkeyword, sizeof(lkeyword), "jp.%s", keyword);
+      attr = ppdFindAttr(ppd, lkeyword, spec);
+    }
   }
 
 #ifdef DEBUG
