207fa5ce78fbde12bdccbd6f009466c984d5baac
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2007-10-10 21:08:51 +0000

    Don't show pap scheme by itself.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@7010 7a7537e8-13f0-0310-91df-b6672ffda945

--- backend/pap.c
+++ backend/pap.c
@@ -207,12 +207,7 @@ int main (int argc, const char * argv[])
 
   if (argc == 1 || (argc == 2 && strcmp(argv[1], "-discover") == 0))
   {
-    /* If listDevices() didn't find any devices or returns an error output a 
-    *  legacy style announcement.
-    *  
-    */
-    if (listDevices() <= 0)
-      puts("network pap \"Unknown\" \"AppleTalk Printer Access Protocol (pap)\"");
+    listDevices();
 
     return 0;
   }
@@ -291,7 +286,7 @@ static int listDevices(void)
   /* Make sure it's okay to use appletalk */
   if (!okayToUseAppleTalk())
   {
-    fprintf(stderr, "ERROR: AppleTalk disabled in System Preferences\n");
+    fprintf(stderr, "INFO: AppleTalk disabled in System Preferences\n");
     return -1;  /* Network is down */
   }
 
