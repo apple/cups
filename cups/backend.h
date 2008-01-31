1d819b31a7a90ba8cbd1ac8e01655058e8e712ef
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2008-01-31 08:37:11 +0000

    Update filter and backend documentation.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@7282 7a7537e8-13f0-0310-91df-b6672ffda945

--- cups/backend.h
+++ cups/backend.h
@@ -3,7 +3,7 @@
  *
  *   Backend definitions for the Common UNIX Printing System (CUPS).
  *
- *   Copyright 2007 by Apple Inc.
+ *   Copyright 2007-2008 by Apple Inc.
  *   Copyright 1997-2005 by Easy Software Products.
  *
  *   These coded instructions, statements, and computer programs are the
@@ -30,7 +30,7 @@
  * Constants...
  */
 
-typedef enum cups_backend_e		/**** Backend exit codes ****/
+enum cups_backend_e			/**** Backend exit codes ****/
 {
   CUPS_BACKEND_OK = 0,			/* Job completed successfully */
   CUPS_BACKEND_FAILED = 1,		/* Job failed, use error-policy */
@@ -38,7 +38,9 @@ typedef enum cups_backend_e		/**** Backend exit codes ****/
   CUPS_BACKEND_HOLD = 3,		/* Job failed, hold job */
   CUPS_BACKEND_STOP = 4,		/* Job failed, stop queue */
   CUPS_BACKEND_CANCEL = 5		/* Job failed, cancel job */
-} cups_backend_t;
+};
+typedef enum cups_backend_e cups_backend_t;
+					/**** Backend exit codes ****/
 
 
 /*
