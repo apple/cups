707bd1e978bfca5ac33cb10794da2b35e8febbdd
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2008-02-23 00:42:43 +0000

    The job-sheets option was not encoded properly (STR #2715)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@7341 7a7537e8-13f0-0310-91df-b6672ffda945

--- cups/encode.c
+++ cups/encode.c
@@ -3,7 +3,7 @@
  *
  *   Option encoding routines for the Common UNIX Printing System (CUPS).
  *
- *   Copyright 2007 by Apple Inc.
+ *   Copyright 2007-2008 by Apple Inc.
  *   Copyright 1997-2007 by Easy Software Products.
  *
  *   These coded instructions, statements, and computer programs are the
@@ -67,6 +67,8 @@ static const _ipp_option_t ipp_options[] =
   { 0, "job-page-limit",	IPP_TAG_INTEGER,	IPP_TAG_JOB },
   { 0, "job-priority",		IPP_TAG_INTEGER,	IPP_TAG_JOB },
   { 0, "job-quota-period",	IPP_TAG_INTEGER,	IPP_TAG_JOB },
+  { 1, "job-sheets",		IPP_TAG_NAME,		IPP_TAG_JOB },
+  { 1, "job-sheets-default",	IPP_TAG_NAME,		IPP_TAG_PRINTER },
   { 0, "job-uuid",		IPP_TAG_URI,		IPP_TAG_JOB },
   { 0, "landscape",		IPP_TAG_BOOLEAN,	IPP_TAG_JOB },
   { 1, "media",			IPP_TAG_KEYWORD,	IPP_TAG_JOB },
