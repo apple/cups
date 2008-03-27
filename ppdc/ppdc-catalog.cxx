a1749f018e8f63b0f2bf6533e46821af566febee
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2008-03-27 20:47:11 +0000

    The PPD compiler tools now support Mac OS X .strings files for localization (STR #2737)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@7407 7a7537e8-13f0-0310-91df-b6672ffda945

--- ppdc/ppdc-catalog.cxx
+++ ppdc/ppdc-catalog.cxx
@@ -31,6 +31,29 @@
 
 
 //
+// Character encodings...
+//
+
+typedef enum
+{
+  PPDC_CS_AUTO,
+  PPDC_CS_UTF8,
+  PPDC_CS_UTF16BE,
+  PPDC_CS_UTF16LE
+} ppdc_cs_t;
+
+
+//
+// Local functions...
+//
+
+static int	get_utf8(char *&ptr);
+static int	get_utf16(cups_file_t *fp, ppdc_cs_t &cs);
+static int	put_utf8(int ch, char *&ptr, char *end);
+static int	put_utf16(cups_file_t *fp, int ch);
+
+
+//
 // 'ppdcCatalog::ppdcCatalog()' - Create a shared message catalog.
 //
 
@@ -155,146 +178,260 @@ ppdcCatalog::load_messages(
   if ((fp = cupsFileOpen(f, "r")) == NULL)
     return (-1);
 
- /*
-  * Read messages from the catalog file until EOF...
-  *
-  * The format is the GNU gettext .po format, which is fairly simple:
-  *
-  *     msgid "some text"
-  *     msgstr "localized text"
-  *
-  * The ID and localized text can span multiple lines using the form:
-  *
-  *     msgid ""
-  *     "some long text"
-  *     msgstr ""
-  *     "localized text spanning "
-  *     "multiple lines"
-  */
-
-  linenum = 0;
-  id[0]   = '\0';
-  str[0]  = '\0';
-
-  while (cupsFileGets(fp, line, sizeof(line)))
+  if ((ptr = strrchr(f, '.')) == NULL)
+    goto unknown_load_format;
+  else if (!strcmp(ptr, ".strings"))
   {
-    linenum ++;
+   /*
+    * Read messages in Mac OS X ".strings" format, which are UTF-16 text
+    * files of the format:
+    *
+    *     "id" = "str";
+    *
+    * Strings files can also contain C-style comments.
+    */
 
-    // Skip blank and comment lines...
-    if (line[0] == '#' || !line[0])
-      continue;
+    ppdc_cs_t	cs = PPDC_CS_AUTO;	// Character set for file
+    int		ch;			// Current character from file
+    char	*end;			// End of buffer
 
-    // Strip the trailing quote...
-    if ((ptr = strrchr(line, '\"')) == NULL)
+
+    id[0]  = '\0';
+    str[0] = '\0';
+    ptr    = NULL;
+    end    = NULL;
+
+    while ((ch = get_utf16(fp, cs)) != 0)
     {
-      fprintf(stderr, "load_messages: Expected quoted string on line %d of %s!\n",
-              linenum, f);
-      cupsFileClose(fp);
-      return (-1);
-    }
-
-    *ptr = '\0';
-
-    // Find start of value...
-    if ((ptr = strchr(line, '\"')) == NULL)
-    {
-      fprintf(stderr, "load_messages: Expected quoted string on line %d of %s!\n",
-              linenum, f);
-      cupsFileClose(fp);
-      return (-1);
-    }
-
-    ptr ++;
-
-    // Unquote the text...
-    char *sptr, *dptr;			// Source/destination pointers
-
-    for (sptr = ptr, dptr = ptr; *sptr;)
-    {
-      if (*sptr == '\\')
+      if (ptr)
       {
-	sptr ++;
-	if (isdigit(*sptr))
+        if (ch == '\\')
 	{
-	  *dptr = 0;
+	  if ((ch = get_utf16(fp, cs)) == 0)
+	    break;
 
-	  while (isdigit(*sptr))
+          if (ch == 'n')
+	    ch = '\n';
+	  else if (ch == 't')
+	    ch = '\t';
+        }
+
+        put_utf8(ch, ptr, end);
+      }
+      else if (ch == '/')
+      {
+        // Start of a comment?
+	if ((ch = get_utf16(fp, cs)) == 0)
+	  break;
+
+        if (ch == '*')
+	{
+	  // Skip C comment...
+	  int lastch = 0;
+
+          while ((ch = get_utf16(fp, cs)) != 0)
 	  {
-	    *dptr = *dptr * 8 + *sptr - '0';
-	    sptr ++;
-	  }
+	    if (ch == '/' && lastch == '*')
+	      break;
 
-	  dptr ++;
+	    lastch = ch;
+	  }
+	}
+	else if (ch == '/')
+	{
+	  // Skip C++ comment...
+	  while ((ch = get_utf16(fp, cs)) != 0)
+	    if (ch == '\n')
+	      break;
+	}
+      }
+      else if (ch == '\"')
+      {
+        // Start or finish quoted string...
+	if (ptr)
+	{
+	  *ptr = '\0';
+	  ptr  = NULL;
+	}
+	else if (id[0])
+	{
+	  ptr = str;
+	  end = str + sizeof(str) - 1;
 	}
 	else
 	{
-	  if (*sptr == 'n')
-	    *dptr++ = '\n';
-	  else if (*sptr == 'r')
-	    *dptr++ = '\r';
-	  else if (*sptr == 't')
-	    *dptr++ = '\t';
-	  else
-	    *dptr++ = *sptr;
-
-	  sptr ++;
+	  ptr = id;
+	  end = id + sizeof(id) - 1;
 	}
       }
-      else
-	*dptr++ = *sptr++;
-    }
-
-    *dptr = '\0';
-
-    // Create or add to a message...
-    if (!strncmp(line, "msgid", 5))
-    {
-      if (id[0] && str[0])
+      else if (ch == ';')
       {
+        // Add string...
 	temp = new ppdcMessage(id, str);
 
 	messages->add(temp);
       }
-
-      strlcpy(id, ptr, sizeof(id));
-      str[0] = '\0';
     }
-    else if (!strncmp(line, "msgstr", 6))
+  }
+  else if (!strcmp(ptr, ".po") || !strcmp(ptr, ".gz"))
+  {
+   /*
+    * Read messages from the catalog file until EOF...
+    *
+    * The format is the GNU gettext .po format, which is fairly simple:
+    *
+    *     msgid "some text"
+    *     msgstr "localized text"
+    *
+    * The ID and localized text can span multiple lines using the form:
+    *
+    *     msgid ""
+    *     "some long text"
+    *     msgstr ""
+    *     "localized text spanning "
+    *     "multiple lines"
+    */
+
+    linenum = 0;
+    id[0]   = '\0';
+    str[0]  = '\0';
+
+    while (cupsFileGets(fp, line, sizeof(line)))
     {
-      if (!id[0])
+      linenum ++;
+
+      // Skip blank and comment lines...
+      if (line[0] == '#' || !line[0])
+	continue;
+
+      // Strip the trailing quote...
+      if ((ptr = strrchr(line, '\"')) == NULL)
       {
-	fprintf(stderr, "load_messages: Need a msgid line before any "
-                	"translation strings on line %d of %s!\n",
+	fprintf(stderr, "ERROR: Expected quoted string on line %d of %s!\n",
 		linenum, f);
 	cupsFileClose(fp);
 	return (-1);
       }
 
-      strlcpy(str, ptr, sizeof(str));
+      *ptr = '\0';
+
+      // Find start of value...
+      if ((ptr = strchr(line, '\"')) == NULL)
+      {
+	fprintf(stderr, "ERROR: Expected quoted string on line %d of %s!\n",
+		linenum, f);
+	cupsFileClose(fp);
+	return (-1);
+      }
+
+      ptr ++;
+
+      // Unquote the text...
+      char *sptr, *dptr;			// Source/destination pointers
+
+      for (sptr = ptr, dptr = ptr; *sptr;)
+      {
+	if (*sptr == '\\')
+	{
+	  sptr ++;
+	  if (isdigit(*sptr))
+	  {
+	    *dptr = 0;
+
+	    while (isdigit(*sptr))
+	    {
+	      *dptr = *dptr * 8 + *sptr - '0';
+	      sptr ++;
+	    }
+
+	    dptr ++;
+	  }
+	  else
+	  {
+	    if (*sptr == 'n')
+	      *dptr++ = '\n';
+	    else if (*sptr == 'r')
+	      *dptr++ = '\r';
+	    else if (*sptr == 't')
+	      *dptr++ = '\t';
+	    else
+	      *dptr++ = *sptr;
+
+	    sptr ++;
+	  }
+	}
+	else
+	  *dptr++ = *sptr++;
+      }
+
+      *dptr = '\0';
+
+      // Create or add to a message...
+      if (!strncmp(line, "msgid", 5))
+      {
+	if (id[0] && str[0])
+	{
+	  temp = new ppdcMessage(id, str);
+
+	  messages->add(temp);
+	}
+
+	strlcpy(id, ptr, sizeof(id));
+	str[0] = '\0';
+      }
+      else if (!strncmp(line, "msgstr", 6))
+      {
+	if (!id[0])
+	{
+	  fprintf(stderr, "ERROR: Need a msgid line before any "
+			  "translation strings on line %d of %s!\n",
+		  linenum, f);
+	  cupsFileClose(fp);
+	  return (-1);
+	}
+
+	strlcpy(str, ptr, sizeof(str));
+      }
+      else if (line[0] == '\"' && str[0])
+	strlcat(str, ptr, sizeof(str));
+      else if (line[0] == '\"' && id[0])
+	strlcat(id, ptr, sizeof(id));
+      else
+      {
+	fprintf(stderr, "ERROR: Unexpected text on line %d of %s!\n",
+		linenum, f);
+	cupsFileClose(fp);
+	return (-1);
+      }
     }
-    else if (line[0] == '\"' && str[0])
-      strlcat(str, ptr, sizeof(str));
-    else if (line[0] == '\"' && id[0])
-      strlcat(id, ptr, sizeof(id));
-    else
+
+    if (id[0] && str[0])
     {
-      fprintf(stderr, "load_messages: Unexpected text on line %d of %s!\n",
-              linenum, f);
-      cupsFileClose(fp);
-      return (-1);
+      temp = new ppdcMessage(id, str);
+
+      messages->add(temp);
     }
   }
+  else
+    goto unknown_load_format;
 
-  if (id[0] && str[0])
-  {
-    temp = new ppdcMessage(id, str);
-
-    messages->add(temp);
-  }
+ /*
+  * Close the file and return...
+  */
 
   cupsFileClose(fp);
 
   return (0);
+
+ /*
+  * Unknown format error...
+  */
+
+  unknown_load_format:
+
+  fprintf(stderr, "ERROR: Unknown message catalog format for \"%s\"!\n", f);
+  cupsFileClose(fp);
+  return (-1);
 }
 
 
@@ -308,60 +445,418 @@ ppdcCatalog::save_messages(
 {
   cups_file_t	*fp;			// Message file
   ppdcMessage	*m;			// Current message
-  const char	*ptr;			// Pointer into string
+  char		*ptr;			// Pointer into string
+  int		utf16;			// Output UTF-16 .strings file?
+  int		ch;			// Current character
 
 
-  if ((fp = cupsFileOpen(f, "w")) == NULL)
+  // Open the file...
+  if ((ptr = strrchr(f, '.')) == NULL)
     return (-1);
 
+  if (!strcmp(ptr, ".gz"))
+    fp = cupsFileOpen(f, "w9");
+  else
+    fp = cupsFileOpen(f, "w");
+
+  if (!fp)
+    return (-1);
+
+  // For .strings files, write a BOM for big-endian output...
+  utf16 = !strcmp(ptr, ".strings");
+
+  if (utf16)
+    put_utf16(fp, 0xfeff);
+
+  // Loop through all of the messages...
   for (m = (ppdcMessage *)messages->first();
        m;
        m = (ppdcMessage *)messages->next())
   {
-    cupsFilePuts(fp, "msgid \"");
-    for (ptr = m->id->value; *ptr; ptr ++)
-      switch (*ptr)
-      {
-        case '\n' :
-	    cupsFilePuts(fp, "\\n");
-	    break;
-        case '\\' :
-	    cupsFilePuts(fp, "\\\\");
-	    break;
-        case '\"' :
-	    cupsFilePuts(fp, "\\\"");
-	    break;
-        default :
-	    cupsFilePutChar(fp, *ptr);
-	    break;
-      }
-    cupsFilePuts(fp, "\"\n");
+    if (utf16)
+    {
+      put_utf16(fp, '\"');
 
-    cupsFilePuts(fp, "msgstr \"");
-    for (ptr = m->string->value; *ptr; ptr ++)
-      switch (*ptr)
-      {
-        case '\n' :
-	    cupsFilePuts(fp, "\\n");
-	    break;
-        case '\\' :
-	    cupsFilePuts(fp, "\\\\");
-	    break;
-        case '\"' :
-	    cupsFilePuts(fp, "\\\"");
-	    break;
-        default :
-	    cupsFilePutChar(fp, *ptr);
-	    break;
-      }
-    cupsFilePuts(fp, "\"\n");
+      ptr = m->id->value;
+      while ((ch = get_utf8(ptr)) != 0)
+	switch (ch)
+	{
+	  case '\n' :
+	      put_utf16(fp, '\\');
+	      put_utf16(fp, 'n');
+	      break;
+	  case '\\' :
+	      put_utf16(fp, '\\');
+	      put_utf16(fp, '\\');
+	      break;
+	  case '\"' :
+	      put_utf16(fp, '\\');
+	      put_utf16(fp, '\"');
+	      break;
+	  default :
+	      put_utf16(fp, ch);
+	      break;
+	}
 
-    cupsFilePutChar(fp, '\n');
+      put_utf16(fp, '\"');
+      put_utf16(fp, ' ');
+      put_utf16(fp, '=');
+      put_utf16(fp, ' ');
+      put_utf16(fp, '\"');
+
+      ptr = m->string->value;
+      while ((ch = get_utf8(ptr)) != 0)
+	switch (ch)
+	{
+	  case '\n' :
+	      put_utf16(fp, '\\');
+	      put_utf16(fp, 'n');
+	      break;
+	  case '\\' :
+	      put_utf16(fp, '\\');
+	      put_utf16(fp, '\\');
+	      break;
+	  case '\"' :
+	      put_utf16(fp, '\\');
+	      put_utf16(fp, '\"');
+	      break;
+	  default :
+	      put_utf16(fp, ch);
+	      break;
+	}
+
+      put_utf16(fp, '\"');
+      put_utf16(fp, ';');
+      put_utf16(fp, '\n');
+    }
+    else
+    {
+      cupsFilePuts(fp, "msgid \"");
+      for (ptr = m->id->value; *ptr; ptr ++)
+	switch (*ptr)
+	{
+	  case '\n' :
+	      cupsFilePuts(fp, "\\n");
+	      break;
+	  case '\\' :
+	      cupsFilePuts(fp, "\\\\");
+	      break;
+	  case '\"' :
+	      cupsFilePuts(fp, "\\\"");
+	      break;
+	  default :
+	      cupsFilePutChar(fp, *ptr);
+	      break;
+	}
+      cupsFilePuts(fp, "\"\n");
+
+      cupsFilePuts(fp, "msgstr \"");
+      for (ptr = m->string->value; *ptr; ptr ++)
+	switch (*ptr)
+	{
+	  case '\n' :
+	      cupsFilePuts(fp, "\\n");
+	      break;
+	  case '\\' :
+	      cupsFilePuts(fp, "\\\\");
+	      break;
+	  case '\"' :
+	      cupsFilePuts(fp, "\\\"");
+	      break;
+	  default :
+	      cupsFilePutChar(fp, *ptr);
+	      break;
+	}
+      cupsFilePuts(fp, "\"\n");
+
+      cupsFilePutChar(fp, '\n');
+    }
   }
 
   cupsFileClose(fp);
 
   return (0);
+}
+
+
+//
+// 'get_utf8()' - Get a UTF-8 character.
+//
+
+static int				// O  - Unicode character or 0 on EOF
+get_utf8(char *&ptr)			// IO - Pointer to character
+{
+  int	ch;				// Current character
+
+
+  if ((ch = *ptr++ & 255) < 0xc0)
+    return (ch);
+
+  if ((ch & 0xe0) == 0xc0)
+  {
+    // Two-byte UTF-8...
+    if ((*ptr & 0xc0) != 0x80)
+      return (0);
+
+    ch = ((ch & 0x1f) << 6) | (*ptr++ & 0x3f);
+  }
+  else if ((ch & 0xf0) == 0xe0)
+  {
+    // Three-byte UTF-8...
+    if ((*ptr & 0xc0) != 0x80)
+      return (0);
+
+    ch = ((ch & 0x0f) << 6) | (*ptr++ & 0x3f);
+
+    if ((*ptr & 0xc0) != 0x80)
+      return (0);
+
+    ch = (ch << 6) | (*ptr++ & 0x3f);
+  }
+  else if ((ch & 0xf8) == 0xf0)
+  {
+    // Four-byte UTF-8...
+    if ((*ptr & 0xc0) != 0x80)
+      return (0);
+
+    ch = ((ch & 0x07) << 6) | (*ptr++ & 0x3f);
+
+    if ((*ptr & 0xc0) != 0x80)
+      return (0);
+
+    ch = (ch << 6) | (*ptr++ & 0x3f);
+
+    if ((*ptr & 0xc0) != 0x80)
+      return (0);
+
+    ch = (ch << 6) | (*ptr++ & 0x3f);
+  }
+
+  return (ch);
+}
+
+
+//
+// 'get_utf16()' - Get a UTF-16 character...
+//
+
+static int				// O  - Unicode character or 0 on EOF
+get_utf16(cups_file_t *fp,		// I  - File to read from
+          ppdc_cs_t   &cs)		// IO - Character set of file
+{
+  int		ch;			// Current character
+  unsigned char	buffer[3];		// Bytes
+
+
+  if (cs == PPDC_CS_AUTO)
+  {
+    // Get byte-order-mark, if present...
+    if (cupsFileRead(fp, (char *)buffer, 2) != 2)
+      return (0);
+
+    if (buffer[0] == 0xfe && buffer[1] == 0xff)
+    {
+      // Big-endian UTF-16...
+      cs = PPDC_CS_UTF16BE;
+
+      if (cupsFileRead(fp, (char *)buffer, 2) != 2)
+	return (0);
+    }
+    else if (buffer[0] == 0xff && buffer[1] == 0xfe)
+    {
+      // Little-endian UTF-16...
+      cs = PPDC_CS_UTF16LE;
+
+      if (cupsFileRead(fp, (char *)buffer, 2) != 2)
+	return (0);
+    }
+    else if (buffer[0] == 0x00 && buffer[1] != 0x00)
+    {
+      // No BOM, assume big-endian UTF-16...
+      cs = PPDC_CS_UTF16BE;
+    }
+    else if (buffer[0] != 0x00 && buffer[1] == 0x00)
+    {
+      // No BOM, assume little-endian UTF-16...
+      cs = PPDC_CS_UTF16LE;
+    }
+    else
+    {
+      // No BOM, assume UTF-8...
+      cs = PPDC_CS_UTF8;
+
+      cupsFileRewind(fp);
+    }
+  }
+  else if (cs != PPDC_CS_UTF8)
+  {
+    if (cupsFileRead(fp, (char *)buffer, 2) != 2)
+      return (0);
+  }
+
+  if (cs == PPDC_CS_UTF8)
+  {
+    // UTF-8 character...
+    ch = cupsFileGetChar(fp);
+
+    if ((ch & 0xe0) == 0xc0)
+    {
+      // Two-byte UTF-8...
+      if (cupsFileRead(fp, (char *)buffer, 1) != 1)
+        return (0);
+
+      if ((buffer[0] & 0xc0) != 0x80)
+        return (0);
+
+      ch = ((ch & 0x1f) << 6) | (buffer[0] & 0x3f);
+    }
+    else if ((ch & 0xf0) == 0xe0)
+    {
+      // Three-byte UTF-8...
+      if (cupsFileRead(fp, (char *)buffer, 2) != 2)
+        return (0);
+
+      if ((buffer[0] & 0xc0) != 0x80 ||
+          (buffer[1] & 0xc0) != 0x80)
+        return (0);
+
+      ch = ((((ch & 0x0f) << 6) | (buffer[0] & 0x3f)) << 6) |
+           (buffer[1] & 0x3f);
+    }
+    else if ((ch & 0xf8) == 0xf0)
+    {
+      // Four-byte UTF-8...
+      if (cupsFileRead(fp, (char *)buffer, 3) != 3)
+        return (0);
+
+      if ((buffer[0] & 0xc0) != 0x80 ||
+          (buffer[1] & 0xc0) != 0x80 ||
+          (buffer[2] & 0xc0) != 0x80)
+        return (0);
+
+      ch = ((((((ch & 0x07) << 6) | (buffer[0] & 0x3f)) << 6) |
+             (buffer[1] & 0x3f)) << 6) | (buffer[2] & 0x3f);
+    }
+  }
+  else
+  {
+    // UTF-16 character...
+    if (cs == PPDC_CS_UTF16BE)
+      ch = (buffer[0] << 8) | buffer[1];
+    else
+      ch = (buffer[1] << 8) | buffer[0];
+
+    if (ch >= 0xd800 && ch <= 0xdbff)
+    {
+      // Handle multi-word encoding...
+      int lch;
+
+      if (cupsFileRead(fp, (char *)buffer, 2) != 2)
+        return (0);
+
+      if (cs == PPDC_CS_UTF16BE)
+	lch = (buffer[0] << 8) | buffer[1];
+      else
+	lch = (buffer[1] << 8) | buffer[0];
+
+      if (lch < 0xdc00 || lch >= 0xdfff)
+        return (0);
+
+      ch = (((ch & 0x3ff) << 10) | (lch & 0x3ff)) + 0x10000;
+    }
+  }
+
+  return (ch);
+}
+
+
+//
+// 'put_utf8()' - Add a UTF-8 character to a string.
+//
+
+static int				// O  - 0 on success, -1 on failure
+put_utf8(int  ch,			// I  - Unicode character
+         char *&ptr,			// IO - String pointer
+	 char *end)			// I  - End of buffer
+{
+  if (ch < 0x80)
+  {
+    // One-byte ASCII...
+    if (ptr >= end)
+      return (-1);
+
+    *ptr++ = ch;
+  }
+  else if (ch < 0x800)
+  {
+    // Two-byte UTF-8...
+    if ((ptr + 1) >= end)
+      return (-1);
+
+    *ptr++ = 0xc0 | (ch >> 6);
+    *ptr++ = 0x80 | (ch & 0x3f);
+  }
+  else if (ch < 0x10000)
+  {
+    // Three-byte UTF-8...
+    if ((ptr + 2) >= end)
+      return (-1);
+
+    *ptr++ = 0xe0 | (ch >> 12);
+    *ptr++ = 0x80 | ((ch >> 6) & 0x3f);
+    *ptr++ = 0x80 | (ch & 0x3f);
+  }
+  else
+  {
+    // Four-byte UTF-8...
+    if ((ptr + 3) >= end)
+      return (-1);
+
+    *ptr++ = 0xf0 | (ch >> 18);
+    *ptr++ = 0x80 | ((ch >> 12) & 0x3f);
+    *ptr++ = 0x80 | ((ch >> 6) & 0x3f);
+    *ptr++ = 0x80 | (ch & 0x3f);
+  }
+
+  return (0);
+}
+
+
+//
+// 'put_utf16()' - Write a UTF-16 character to a file.
+//
+
+static int				// O - 0 on success, -1 on failure
+put_utf16(cups_file_t *fp,		// I - File to write to
+          int         ch)		// I - Unicode character
+{
+  unsigned char	buffer[4];		// Output buffer
+
+
+  if (ch < 0x10000)
+  {
+    // One-word UTF-16 big-endian...
+    buffer[0] = ch >> 8;
+    buffer[1] = ch;
+
+    if (cupsFileWrite(fp, (char *)buffer, 2) == 2)
+      return (0);
+  }
+  else
+  {
+    // Two-word UTF-16 big-endian...
+    ch -= 0x10000;
+
+    buffer[0] = 0xd8 | (ch >> 18);
+    buffer[1] = ch >> 10;
+    buffer[2] = 0xdc | ((ch >> 8) & 0x03);
+    buffer[3] = ch;
+
+    if (cupsFileWrite(fp, (char *)buffer, 4) == 4)
+      return (0);
+  }
+
+  return (-1);
 }
 
 
