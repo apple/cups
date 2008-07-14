80c55d999df74b753ef2a061441c2b8d0a585aff
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2008-07-14 23:04:27 +0000

    Don't try to print files with 0 or 1 bytes.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@7737 7a7537e8-13f0-0310-91df-b6672ffda945

--- backend/ipp.c
+++ backend/ipp.c
@@ -405,6 +405,7 @@ main(int  argc,				/* I - Number of command-line args */
     cups_file_t	*fp;			/* Temporary file */
     char	buffer[8192];		/* Buffer for copying */
     int		bytes;			/* Number of bytes read */
+    off_t	tbytes;			/* Total bytes copied */
 
 
     if ((fd = cupsTempFd(tmpfilename, sizeof(tmpfilename))) < 0)
@@ -421,6 +422,8 @@ main(int  argc,				/* I - Number of command-line args */
       return (CUPS_BACKEND_FAILED);
     }
 
+    tbytes = 0;
+
     while ((bytes = fread(buffer, 1, sizeof(buffer), stdin)) > 0)
       if (cupsFileWrite(fp, buffer, bytes) < bytes)
       {
@@ -429,8 +432,21 @@ main(int  argc,				/* I - Number of command-line args */
 	unlink(tmpfilename);
 	return (CUPS_BACKEND_FAILED);
       }
+      else
+        tbytes += bytes;
 
     cupsFileClose(fp);
+
+   /*
+    * Don't try printing files less than 2 bytes...
+    */
+
+    if (tbytes <= 1)
+    {
+      _cupsLangPuts(_("ERROR: Empty print file!\n"));
+      unlink(tmpfilename);
+      return (CUPS_BACKEND_FAILED);
+    }
 
    /*
     * Point to the single file from stdin...
