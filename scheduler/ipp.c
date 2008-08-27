a98f678102afc26164c50d981b9137d85eed378c
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2008-08-27 23:23:18 +0000

    Also check that printer->info is not the empty string.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@7877 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/ipp.c
+++ scheduler/ipp.c
@@ -25,7 +25,7 @@
  *   add_job()                   - Add a job to a print queue.
  *   add_job_state_reasons()     - Add the "job-state-reasons" attribute based
  *                                 upon the job and printer state...
- *   add_job_subscriptions()     - Add any subcriptions for a job.
+ *   add_job_subscriptions()     - Add any subscriptions for a job.
  *   add_job_uuid()              - Add job-uuid attribute to a job.
  *   add_printer()               - Add a printer to the system.
  *   add_printer_state_reasons() - Add the "printer-state-reasons" attribute
@@ -1978,7 +1978,7 @@ add_job_state_reasons(
 
 
 /*
- * 'add_job_subscriptions()' - Add any subcriptions for a job.
+ * 'add_job_subscriptions()' - Add any subscriptions for a job.
  */
 
 static void
@@ -3394,7 +3394,8 @@ apple_register_profiles(
 					     &kCFTypeDictionaryKeyCallBacks,
 					     &kCFTypeDictionaryValueCallBacks);
     printer_name = CFStringCreateWithCString(kCFAllocatorDefault,
-                                             p->info ? p->info : p->name,
+                                             p->info && p->info[0] ? p->info :
+					                             p->name,
 					     kCFStringEncodingUTF8);
 
     if (device_name && printer_name)
