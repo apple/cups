e2607122ec92919654675fdf194edde1bab5c5be
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2009-05-13 17:20:29 +0000

    Don't localize "paused" or "moving to paused"
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@8623 7a7537e8-13f0-0310-91df-b6672ffda945

--- cups/localize.c
+++ cups/localize.c
@@ -308,9 +308,7 @@ ppdLocalizeIPPReason(
         message = _("Media tray needs to be filled.");
       else if (!strncmp(reason, "media-jam", 9))
         message = _("Media jam!");
-      else if (!strncmp(reason, "moving-to-paused", 16) ||
-	       !strncmp(reason, "offline", 7) ||
-	       !strncmp(reason, "paused", 6) ||
+      else if (!strncmp(reason, "offline", 7) ||
 	       !strncmp(reason, "shutdown", 8))
         message = _("Printer offline.");
       else if (!strncmp(reason, "toner-low", 9))
