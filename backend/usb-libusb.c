1ac626aa7306a9191371adbbc443f628c474fd94
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2009-09-11 20:03:21 +0000

    USB process hangs on end-of-file, making further printing impossible (STR #3318)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@8812 7a7537e8-13f0-0310-91df-b6672ffda945

--- backend/usb-libusb.c
+++ backend/usb-libusb.c
@@ -158,7 +158,16 @@ print_device(const char *uri,		/* I - Device URI */
 
     while (poll(pfds, 2, -1) > 0)
     {
-      if (pfds[0].revents & POLLIN)
+     /*
+      * CUPS STR #3318: USB process hangs on end-of-file, making further
+      *                 printing impossible
+      *
+      * From a strict interpretation of POSIX poll(), POLLHUP should never be
+      * set without POLLIN, since POLLIN is the event you request.  That said,
+      * it appears that some versions of Linux break this.
+      */
+
+      if (pfds[0].revents & (POLLIN | POLLHUP))
       {
 	if ((bytes = read(print_fd, buffer, sizeof(buffer))) > 0)
 	{
@@ -178,7 +187,7 @@ print_device(const char *uri,		/* I - Device URI */
 	  break;
       }
 
-      if (pfds[1].revents & POLLIN)
+      if (pfds[1].revents & (POLLIN | POLLHUP))
       {
         if ((bytes = side_cb(printer, print_fd)) < 0)
 	  pfds[1].events = 0;		/* Filter has gone away... */
