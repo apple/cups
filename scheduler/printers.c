42ca09de1407ee0807fc55b56bcae9f8096e85b8
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2011-01-21 22:52:43 +0000

    Provide default values for pages-per-minute and pages-per-minute-color.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@9501 7a7537e8-13f0-0310-91df-b6672ffda945

--- scheduler/printers.c
+++ scheduler/printers.c
@@ -4120,6 +4120,18 @@ load_ppd(cupsd_printer_t *p)		/* I - Printer */
 	ippAddInteger(p->ppd_attrs, IPP_TAG_PRINTER, IPP_TAG_INTEGER,
 		      "pages-per-minute-color", ppd->throughput);
     }
+    else
+    {
+     /*
+      * When there is no speed information, just say "1 page per minute".
+      */
+
+      ippAddInteger(p->ppd_attrs, IPP_TAG_PRINTER, IPP_TAG_INTEGER,
+		    "pages-per-minute", 1);
+      if (ppd->color_device)
+	ippAddInteger(p->ppd_attrs, IPP_TAG_PRINTER, IPP_TAG_INTEGER,
+		      "pages-per-minute-color", 1);
+    }
 
     num_qualities = 0;
 
