38e5e941b2ac81e0788a62f6d7c3e97723ddc28b
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2011-10-03 17:27:45 +0000

    The DBUS notifier could crash (STR #3947)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@10041 7a7537e8-13f0-0310-91df-b6672ffda945

--- notifier/dbus.c
+++ notifier/dbus.c
@@ -4,7 +4,7 @@
  *   D-Bus notifier for CUPS.
  *
  *   Copyright 2008-2010 by Apple Inc.
- *   Copyright (C) 2007 Red Hat, Inc.
+ *   Copyright (C) 2011 Red Hat, Inc.
  *   Copyright (C) 2007 Tim Waugh <twaugh@redhat.com>
  *   Copyright 1997-2005 by Easy Software Products.
  *
@@ -353,7 +353,7 @@ main(int  argc,				/* I - Number of command-line args */
     /*
      * Create and send the new message...
      */
-    
+
     fprintf(stderr, "DEBUG: %s\n", signame);
     message = dbus_message_new_signal("/org/cups/cupsd/Notifier",
 				      "org.cups.cupsd.Notifier",
@@ -423,10 +423,11 @@ main(int  argc,				/* I - Number of command-line args */
 	  p = printer_reasons;
 	  for (i = 0; i < attr->num_values; i++)
 	  {
-	    strcpy(p, attr->values[i].string.text);
-	    p += strlen(p);
 	    if (i)
 	      *p++ = ',';
+
+	    strcpy(p, attr->values[i].string.text);
+	    p += strlen(p);
 	  }
 	  dbus_message_iter_append_string(&iter, &printer_reasons);
 	}
