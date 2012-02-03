f20fa9160100655ac888ef06d4f8aac26241f3f7
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2012-02-03 21:39:42 +0000

    Don't echo comment line (sigh...)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@10223 7a7537e8-13f0-0310-91df-b6672ffda945

--- vcnet/setdebug.bat
+++ vcnet/setdebug.bat
@@ -1,4 +1,4 @@
-rem Script to enable debug logging for IPPTOOL
+@rem Script to enable debug logging for IPPTOOL
 set CUPS_DEBUG_LOG=ipptool.log
 set CUPS_DEBUG_LEVEL=6
 set "CUPS_DEBUG_FILTER=^(http|_http|ipp|_ipp|cupsDo|cupsGetResponse|cupsSend)"
