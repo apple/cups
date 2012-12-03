ec39bf1a0eaa1dd44d3238778a217334e6e19a23
mike <mike@7a7537e8-13f0-0310-91df-b6672ffda945>
2012-12-03 13:30:07 +0000

    Fix interop with older CUPS servers by falling back to IPP/1.1 (STR #4231)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@10722 7a7537e8-13f0-0310-91df-b6672ffda945

--- cups/globals.c
+++ cups/globals.c
@@ -214,11 +214,12 @@ cups_globals_alloc(void)
   */
 
   memset(cg, 0, sizeof(_cups_globals_t));
-  cg->encryption    = (http_encryption_t)-1;
-  cg->password_cb   = (cups_password_cb2_t)_cupsGetPassword;
-  cg->any_root      = 1;
-  cg->expired_certs = 1;
-  cg->expired_root  = 1;
+  cg->encryption     = (http_encryption_t)-1;
+  cg->password_cb    = (cups_password_cb2_t)_cupsGetPassword;
+  cg->any_root       = 1;
+  cg->expired_certs  = 1;
+  cg->expired_root   = 1;
+  cg->server_version = 20;
 
 #ifdef DEBUG
  /*
