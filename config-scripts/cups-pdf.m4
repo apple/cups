75c4e7f14f33dd604cebd31be214b645e39cc465
msweet <msweet@a1ca3aef-8c08-0410-bb20-df032aa958be>
2013-05-10 18:56:23 +0000

    Import cups.org releases
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/tags/release-1.5b2@4306 a1ca3aef-8c08-0410-bb20-df032aa958be

---
+++ config-scripts/cups-pdf.m4
@@ -0,0 +1,113 @@
+dnl
+dnl "$Id: cups-pdf.m4 9750 2011-05-06 22:53:53Z mike $"
+dnl
+dnl   PDF filter configuration stuff for CUPS.
+dnl
+dnl   Copyright 2007-2011 by Apple Inc.
+dnl   Copyright 2006 by Easy Software Products, all rights reserved.
+dnl
+dnl   These coded instructions, statements, and computer programs are the
+dnl   property of Apple Inc. and are protected by Federal copyright
+dnl   law.  Distribution and use rights are outlined in the file "LICENSE.txt"
+dnl   which should have been included with this file.  If this file is
+dnl   file is missing or damaged, see the license at "http://www.cups.org/".
+dnl
+
+AC_ARG_WITH(pdftops, [  --with-pdftops          set pdftops filter (gs,/path/to/gs,pdftops,/path/to/pdftops,none), default=pdftops ])
+
+PDFTOPS=""
+CUPS_PDFTOPS=""
+CUPS_GHOSTSCRIPT=""
+
+case "x$with_pdftops" in
+	x) # Default/auto
+	if test $uname != Darwin; then
+		AC_PATH_PROG(CUPS_PDFTOPS, pdftops)
+		if test "x$CUPS_PDFTOPS" != x; then
+			AC_DEFINE(HAVE_PDFTOPS)
+			PDFTOPS="pdftops"
+		else
+			AC_PATH_PROG(CUPS_GHOSTSCRIPT, gs)
+			if test "x$CUPS_GHOSTSCRIPT" != x; then
+				AC_DEFINE(HAVE_GHOSTSCRIPT)
+				PDFTOPS="pdftops"
+			fi
+		fi
+	fi
+	;;
+
+	xgs)
+	AC_PATH_PROG(CUPS_GHOSTSCRIPT, gs)
+	if test "x$CUPS_GHOSTSCRIPT" != x; then
+		AC_DEFINE(HAVE_GHOSTSCRIPT)
+		PDFTOPS="pdftops"
+	else
+		AC_MSG_ERROR(Unable to find gs program!)
+		exit 1
+	fi
+	;;
+
+	x/*/gs) # Use /path/to/gs without any check:
+	CUPS_GHOSTSCRIPT="$with_pdftops"
+	AC_DEFINE(HAVE_GHOSTSCRIPT)
+	PDFTOPS="pdftops"
+	;;
+
+	xpdftops)
+	AC_PATH_PROG(CUPS_PDFTOPS, pdftops)
+	if test "x$CUPS_PDFTOPS" != x; then
+		AC_DEFINE(HAVE_PDFTOPS)
+		PDFTOPS="pdftops"
+	else
+		AC_MSG_ERROR(Unable to find pdftops program!)
+		exit 1
+	fi
+	;;
+
+	x/*/pdftops) # Use /path/to/pdftops without any check:
+	CUPS_PDFTOPS="$with_pdftops"
+	AC_DEFINE(HAVE_PDFTOPS)
+	PDFTOPS="pdftops"
+	;;
+
+	xnone) # Make no pdftops filter if with_pdftops=none:
+	;;
+
+	*) # Invalid with_pdftops value:
+	AC_MSG_ERROR(Invalid with_pdftops value!)
+	exit 1
+	;;
+esac
+
+if test "x$CUPS_PDFTOPS" != x; then
+	AC_MSG_CHECKING(whether pdftops supports -origpagesizes)
+	if ($CUPS_PDFTOPS -h 2>&1 | grep -q -- -origpagesizes); then
+		AC_MSG_RESULT(yes)
+		AC_DEFINE(HAVE_PDFTOPS_WITH_ORIGPAGESIZES)
+	else
+		AC_MSG_RESULT(no)
+	fi
+
+	DEFAULT_PDFTOPS=""
+elif test "x$CUPS_GHOSTSCRIPT" != x; then
+	AC_MSG_CHECKING(whether gs supports the ps2write device)
+	if ($CUPS_GHOSTSCRIPT -h 2>&1 | grep -q ps2write); then
+		AC_MSG_RESULT(yes)
+		AC_DEFINE(HAVE_GHOSTSCRIPT_PS2WRITE)
+	else
+		AC_MSG_RESULT(no)
+	fi
+
+	DEFAULT_PDFTOPS=""
+else
+	DEFAULT_PDFTOPS="#"
+fi
+
+AC_DEFINE_UNQUOTED(CUPS_PDFTOPS, "$CUPS_PDFTOPS")
+AC_DEFINE_UNQUOTED(CUPS_GHOSTSCRIPT, "$CUPS_GHOSTSCRIPT")
+AC_SUBST(DEFAULT_PDFTOPS)
+AC_SUBST(PDFTOPS)
+
+dnl
+dnl End of "$Id: cups-pdf.m4 9750 2011-05-06 22:53:53Z mike $".
+dnl
---
+++ vcnet/config.h
@@ -0,0 +1,788 @@
+/*
+ * "$Id$"
+ *
+ *   Configuration file for CUPS on Windows.
+ *
+ *   Copyright 2007-2011 by Apple Inc.
+ *   Copyright 1997-2007 by Easy Software Products.
+ *
+ *   These coded instructions, statements, and computer programs are the
+ *   property of Apple Inc. and are protected by Federal copyright
+ *   law.  Distribution and use rights are outlined in the file "LICENSE.txt"
+ *   which should have been included with this file.  If this file is
+ *   file is missing or damaged, see the license at "http://www.cups.org/".
+ */
+
+#ifndef _CUPS_CONFIG_H_
+#define _CUPS_CONFIG_H_
+
+/*
+ * Include necessary headers...
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <io.h>
+#include <direct.h>
+
+
+/*
+ * Microsoft renames the POSIX functions to _name, and introduces
+ * a broken compatibility layer using the original names.  As a result,
+ * random crashes can occur when, for example, strdup() allocates memory
+ * from a different heap than used by malloc() and free().
+ *
+ * To avoid moronic problems like this, we #define the POSIX function
+ * names to the corresponding non-standard Microsoft names.
+ */
+
+#define access		_access
+#define close		_close
+#define fileno		_fileno
+#define lseek		_lseek
+#define mkdir(d,p)	_mkdir(d)
+#define open		_open
+#define read	        _read
+#define rmdir		_rmdir
+#define snprintf 	_snprintf
+#define strdup		_strdup
+#define unlink		_unlink
+#define vsnprintf 	_vsnprintf
+#define write		_write
+
+
+/*
+ * Map the POSIX sleep() and usleep() functions to the Win32 Sleep() function...
+ */
+
+#define sleep(X)	Sleep(1000 * (X))
+#define usleep(X)	Sleep((X)/1000)
+
+
+/*
+ * Map various parameters to Posix style system calls
+ */
+
+#  define F_OK		00
+#  define W_OK		02
+#  define R_OK		04
+#  define O_RDONLY	_O_RDONLY
+#  define O_WRONLY	_O_WRONLY
+#  define O_CREATE	_O_CREAT
+#  define O_TRUNC	_O_TRUNC
+
+
+/*
+ * Compiler stuff...
+ */
+
+#undef const
+#undef __CHAR_UNSIGNED__
+
+
+/*
+ * Version of software...
+ */
+
+#define CUPS_SVERSION "CUPS v1.5.0"
+#define CUPS_MINIMAL "CUPS/1.5.0"
+
+
+/*
+ * Default user and groups...
+ */
+
+#define CUPS_DEFAULT_USER	""
+#define CUPS_DEFAULT_GROUP	""
+#define CUPS_DEFAULT_SYSTEM_GROUPS ""
+#define CUPS_DEFAULT_PRINTOPERATOR_AUTH ""
+
+
+/*
+ * Default file permissions...
+ */
+
+#define CUPS_DEFAULT_CONFIG_FILE_PERM 0644
+#define CUPS_DEFAULT_LOG_FILE_PERM 0644
+
+
+/*
+ * Default logging settings...
+ */
+
+#define CUPS_DEFAULT_LOG_LEVEL "warn"
+#define CUPS_DEFAULT_ACCESS_LOG_LEVEL "actions"
+
+
+/*
+ * Default fatal error settings...
+ */
+
+#define CUPS_DEFAULT_FATAL_ERRORS "config"
+
+
+/*
+ * Default browsing settings...
+ */
+
+#define CUPS_DEFAULT_BROWSING 1
+#define CUPS_DEFAULT_BROWSE_LOCAL_PROTOCOLS "CUPS dnssd"
+#define CUPS_DEFAULT_BROWSE_REMOTE_PROTOCOLS ""
+#define CUPS_DEFAULT_BROWSE_SHORT_NAMES 1
+#define CUPS_DEFAULT_DEFAULT_SHARED 1
+#define CUPS_DEFAULT_IMPLICIT_CLASSES 1
+#define CUPS_DEFAULT_USE_NETWORK_DEFAULT 0
+
+
+/*
+ * Default IPP port...
+ */
+
+#define CUPS_DEFAULT_IPP_PORT 631
+
+
+/*
+ * Default printcap file...
+ */
+
+#define CUPS_DEFAULT_PRINTCAP ""
+
+
+/*
+ * Default Samba and LPD config files...
+ */
+
+#define CUPS_DEFAULT_SMB_CONFIG_FILE ""
+#define CUPS_DEFAULT_LPD_CONFIG_FILE ""
+
+
+/*
+ * Default MaxCopies value...
+ */
+
+#define CUPS_DEFAULT_MAX_COPIES 9999
+
+
+/*
+ * Do we have domain socket support?
+ */
+
+#undef CUPS_DEFAULT_DOMAINSOCKET
+
+
+/*
+ * Where are files stored?
+ *
+ * Note: These are defaults, which can be overridden by environment
+ *       variables at run-time...
+ */
+
+#define CUPS_BINDIR "C:/CUPS/bin"
+#define CUPS_CACHEDIR "C:/CUPS/cache"
+#define CUPS_DATADIR "C:/CUPS/share"
+#define CUPS_DOCROOT "C:/CUPS/share/doc"
+#define CUPS_FONTPATH "C:/CUPS/share/fonts"
+#define CUPS_LOCALEDIR "C:/CUPS/locale"
+#define CUPS_LOGDIR "C:/CUPS/logs"
+#define CUPS_REQUESTS "C:/CUPS/spool"
+#define CUPS_SBINDIR "C:/CUPS/sbin"
+#define CUPS_SERVERBIN "C:/CUPS/lib"
+#define CUPS_SERVERROOT "C:/CUPS/etc"
+#define CUPS_STATEDIR "C:/CUPS/run"
+
+
+/*
+ * Do we have various image libraries?
+ */
+
+/* #undef HAVE_LIBPNG */
+/* #undef HAVE_LIBZ */
+/* #undef HAVE_LIBJPEG */
+/* #undef HAVE_LIBTIFF */
+
+
+/*
+ * Do we have PAM stuff?
+ */
+
+#ifndef HAVE_LIBPAM
+#define HAVE_LIBPAM 0
+#endif /* !HAVE_LIBPAM */
+
+/* #undef HAVE_PAM_PAM_APPL_H */
+/* #undef HAVE_PAM_SET_ITEM */
+/* #undef HAVE_PAM_SETCRED */
+
+
+/*
+ * Do we have <shadow.h>?
+ */
+
+/* #undef HAVE_SHADOW_H */
+
+
+/*
+ * Do we have <crypt.h>?
+ */
+
+/* #undef HAVE_CRYPT_H */
+
+
+/*
+ * Do we have <scsi/sg.h>?
+ */
+
+/* #undef HAVE_SCSI_SG_H */
+
+
+/*
+ * Use <string.h>, <strings.h>, and/or <bstring.h>?
+ */
+
+#define HAVE_STRING_H 1
+/* #undef HAVE_STRINGS_H */
+/* #undef HAVE_BSTRING_H */
+
+
+/*
+ * Do we have the long long type?
+ */
+
+/* #undef HAVE_LONG_LONG */
+
+#ifdef HAVE_LONG_LONG
+#  define CUPS_LLFMT	"%lld"
+#  define CUPS_LLCAST	(long long)
+#else
+#  define CUPS_LLFMT	"%ld"
+#  define CUPS_LLCAST	(long)
+#endif /* HAVE_LONG_LONG */
+
+
+/*
+ * Do we have the strtoll() function?
+ */
+
+/* #undef HAVE_STRTOLL */
+
+#ifndef HAVE_STRTOLL
+#  define strtoll(nptr,endptr,base) strtol((nptr), (endptr), (base))
+#endif /* !HAVE_STRTOLL */
+
+
+/*
+ * Do we have the strXXX() functions?
+ */
+
+#define HAVE_STRDUP 1
+/* #undef HAVE_STRLCAT */
+/* #undef HAVE_STRLCPY */
+
+
+/*
+ * Do we have the geteuid() function?
+ */
+
+/* #undef HAVE_GETEUID */
+
+
+/*
+ * Do we have the setpgid() function?
+ */
+
+/* #undef HAVE_SETPGID */
+
+
+/*
+ * Do we have the vsyslog() function?
+ */
+
+/* #undef HAVE_VSYSLOG */
+
+
+/*
+ * Do we have the (v)snprintf() functions?
+ */
+
+#define HAVE_SNPRINTF 1
+#define HAVE_VSNPRINTF 1
+
+
+/*
+ * What signal functions to use?
+ */
+
+/* #undef HAVE_SIGSET */
+/* #undef HAVE_SIGACTION */
+
+
+/*
+ * What wait functions to use?
+ */
+
+/* #undef HAVE_WAITPID */
+/* #undef HAVE_WAIT3 */
+
+
+/*
+ * Do we have the mallinfo function and malloc.h?
+ */
+
+/* #undef HAVE_MALLINFO */
+/* #undef HAVE_MALLOC_H */
+
+
+/*
+ * Do we have the POSIX ACL functions?
+ */
+
+/* #undef HAVE_ACL_INIT */
+
+
+/*
+ * Do we have the langinfo.h header file?
+ */
+
+/* #undef HAVE_LANGINFO_H */
+
+
+/*
+ * Which encryption libraries do we have?
+ */
+
+/* #undef HAVE_CDSASSL */
+/* #undef HAVE_GNUTLS */
+/* #undef HAVE_LIBSSL */
+#define HAVE_SSPISSL
+#define HAVE_SSL
+
+
+/*
+ * What Security framework headers do we have?
+ */
+
+/* #undef HAVE_AUTHORIZATION_H */
+/* #undef HAVE_SECPOLICY_H */
+/* #undef HAVE_SECPOLICYPRIV_H */
+/* #undef HAVE_SECBASEPRIV_H */
+/* #undef HAVE_SECIDENTITYSEARCHPRIV_H */
+
+
+/*
+ * Do we have the SecIdentitySearchCreateWithPolicy function?
+ */
+
+/* #undef HAVE_SECIDENTITYSEARCHCREATEWITHPOLICY */
+
+
+/*
+ * Do we have the SecPolicyCreateSSL function?
+ */
+
+/* #undef HAVE_SECPOLICYCREATESSL */
+
+
+/*
+ * Do we have the SecPolicyCreateSSL function?
+ */
+
+/* #undef HAVE_SECPOLICYCREATESSL */
+
+
+/*
+ * Do we have the cssmErrorString function?
+ */
+
+/* #undef HAVE_CSSMERRORSTRING */
+
+
+/*
+ * Do we have the SLP library?
+ */
+
+/* #undef HAVE_LIBSLP */
+
+
+/*
+ * Do we have an LDAP library?
+ */
+
+/* #undef HAVE_LDAP */
+/* #undef HAVE_OPENLDAP */
+/* #undef HAVE_MOZILLA_LDAP */
+/* #undef HAVE_LDAP_SSL_H */
+/* #undef HAVE_LDAP_SSL */
+/* #undef HAVE_LDAP_REBIND_PROC */
+
+
+/*
+ * Do we have libpaper?
+ */
+
+/* #undef HAVE_LIBPAPER */
+
+
+/*
+ * Do we have DNS Service Discovery (aka Bonjour)?
+ */
+
+#define HAVE_DNSSD 1
+
+
+/*
+ * Does the "stat" structure contain the "st_gen" member?
+ */
+
+/* #undef HAVE_ST_GEN */
+
+
+/*
+ * Do we have <sys/ioctl.h>?
+ */
+
+/* #undef HAVE_SYS_IOCTL_H */
+
+
+/*
+ * Does the "tm" structure contain the "tm_gmtoff" member?
+ */
+
+/* #undef HAVE_TM_GMTOFF */
+
+
+/*
+ * Do we have rresvport_af()?
+ */
+
+/* #undef HAVE_RRESVPORT_AF */
+
+
+/*
+ * Do we have getaddrinfo()?
+ */
+
+#define HAVE_GETADDRINFO 1
+
+
+/*
+ * Do we have getnameinfo()?
+ */
+
+#define HAVE_GETNAMEINFO 1
+
+
+/*
+ * Do we have getifaddrs()?
+ */
+
+/* #undef HAVE_GETIFADDRS */
+
+
+/*
+ * Do we have hstrerror()?
+ */
+
+/* #undef HAVE_HSTRERROR */
+
+
+/*
+ * Do we have res_init()?
+ */
+
+/* #undef HAVE_RES_INIT */
+
+
+/*
+ * Do we have <resolv.h>
+ */
+
+/* #undef HAVE_RESOLV_H */
+
+
+/*
+ * Do we have the <sys/sockio.h> header file?
+ */
+
+/* #undef HAVE_SYS_SOCKIO_H */
+
+
+/*
+ * Does the sockaddr structure contain an sa_len parameter?
+ */
+
+/* #undef HAVE_STRUCT_SOCKADDR_SA_LEN */
+
+
+/*
+ * Do we have the AIX usersec.h header file?
+ */
+
+/* #undef HAVE_USERSEC_H */
+
+
+/*
+ * Do we have pthread support?
+ */
+
+/* #undef HAVE_PTHREAD_H */
+
+
+/*
+ * Do we have launchd support?
+ */
+
+/* #undef HAVE_LAUNCH_H */
+/* #undef HAVE_LAUNCHD */
+
+
+/*
+ * Various scripting languages...
+ */
+
+/* #undef HAVE_JAVA */
+#define CUPS_JAVA	""
+/* #undef HAVE_PERL */
+#define CUPS_PERL	""
+/* #undef HAVE_PHP */
+#define CUPS_PHP	""
+/* #undef HAVE_PYTHON */
+#define CUPS_PYTHON	""
+
+
+/*
+ * Location of the poppler/Xpdf pdftops program...
+ */
+
+/* #undef HAVE_PDFTOPS */
+#define CUPS_PDFTOPS ""
+
+
+/*
+ * Location of the Ghostscript gs program...
+ */
+
+/* #undef HAVE_GHOSTSCRIPT */
+#define CUPS_GHOSTSCRIPT ""
+
+
+/*
+ * Do we have Darwin's CoreFoundation and SystemConfiguration frameworks?
+ */
+
+/* #undef HAVE_COREFOUNDATION */
+/* #undef HAVE_SYSTEMCONFIGURATION */
+
+
+/*
+ * Do we have CoreFoundation public and private headers?
+ */
+
+/* #undef HAVE_COREFOUNDATION_H */
+/* #undef HAVE_CFPRIV_H */
+/* #undef HAVE_CFBUNDLEPRIV_H */
+
+
+/*
+ * Do we have ApplicationServices public headers?
+ */
+
+/* #undef HAVE_APPLICATIONSERVICES_H */
+
+
+/*
+ * Do we have the SCDynamicStoreCopyComputerName function?
+ */
+
+/* #undef HAVE_SCDYNAMICSTORECOPYCOMPUTERNAME */
+
+
+/*
+ * Do we have Mac OS X 10.4's mbr_XXX functions?
+ */
+
+/* #undef HAVE_MEMBERSHIP_H */
+/* #undef HAVE_MEMBERSHIPPRIV_H */
+/* #undef HAVE_MBR_UID_TO_UUID */
+
+
+/*
+ * Do we have Darwin's notify_post header and function?
+ */
+
+/* #undef HAVE_NOTIFY_H */
+/* #undef HAVE_NOTIFY_POST */
+
+
+/*
+ * Do we have Darwin's IOKit private headers?
+ */
+
+/* #undef HAVE_IOKIT_PWR_MGT_IOPMLIBPRIVATE_H */
+
+
+/*
+ * Do we have DBUS?
+ */
+
+/* #undef HAVE_DBUS */
+/* #undef HAVE_DBUS_MESSAGE_ITER_INIT_APPEND */
+
+
+/*
+ * Do we have the GSSAPI support library (for Kerberos support)?
+ */
+
+/* #undef HAVE_GSS_ACQUIRE_CRED_EX_F */
+/* #undef HAVE_GSS_C_NT_HOSTBASED_SERVICE */
+/* #undef HAVE_GSS_GSSAPI_H */
+/* #undef HAVE_GSS_GSSAPI_SPI_H */
+/* #undef HAVE_GSSAPI */
+/* #undef HAVE_GSSAPI_GENERIC_H */
+/* #undef HAVE_GSSAPI_GSSAPI_H */
+/* #undef HAVE_GSSAPI_H */
+/* #undef HAVE_GSSAPI_KRB5_H */
+/* #undef HAVE_KRB5_H */
+
+
+/*
+ * Default GSS service name...
+ */
+
+#define CUPS_DEFAULT_GSSSERVICENAME "host"
+
+
+/*
+ * Select/poll interfaces...
+ */
+
+/* #undef HAVE_POLL */
+/* #undef HAVE_EPOLL */
+/* #undef HAVE_KQUEUE */
+
+
+/*
+ * Do we have the <dlfcn.h> header?
+ */
+
+/* #undef HAVE_DLFCN_H */
+
+
+/*
+ * Do we have <sys/param.h>?
+ */
+
+/* #undef HAVE_SYS_PARAM_H */
+
+
+/*
+ * Do we have <sys/ucred.h>?
+ */
+
+/* #undef HAVE_SYS_UCRED_H */
+
+
+/*
+ * Do we have removefile()?
+ */
+
+/* #undef HAVE_REMOVEFILE */
+
+
+/*
+ * Do we have <sandbox.h>?
+ */
+
+/* #undef HAVE_SANDBOX_H */
+
+
+/*
+ * Which random number generator function to use...
+ */
+
+/* #undef HAVE_ARC4RANDOM */
+/* #undef HAVE_RANDOM */
+/* #undef HAVE_LRAND48 */
+
+#ifdef HAVE_ARC4RANDOM
+#  define CUPS_RAND() arc4random()
+#  define CUPS_SRAND(v) arc4random_stir()
+#elif defined(HAVE_RANDOM)
+#  define CUPS_RAND() random()
+#  define CUPS_SRAND(v) srandom(v)
+#elif defined(HAVE_LRAND48)
+#  define CUPS_RAND() lrand48()
+#  define CUPS_SRAND(v) srand48(v)
+#else
+#  define CUPS_RAND() rand()
+#  define CUPS_SRAND(v) srand(v)
+#endif /* HAVE_ARC4RANDOM */
+
+
+/*
+ * Do we have vproc_transaction_begin/end?
+ */
+
+/* #undef HAVE_VPROC_TRANSACTION_BEGIN */
+
+
+/*
+ * Do we have libusb?
+ */
+
+/* #undef HAVE_USB_H */
+
+
+/*
+ * Do we have libwrap and tcpd.h?
+ */
+
+/* #undef HAVE_TCPD_H */
+
+
+/*
+ * Do we have <iconv.h>?
+ */
+
+/* #undef HAVE_ICONV_H */
+
+
+/*
+ * Do we have statfs or statvfs and one of the corresponding headers?
+ */
+
+/* #undef HAVE_STATFS */
+/* #undef HAVE_STATVFS */
+/* #undef HAVE_SYS_MOUNT_H */
+/* #undef HAVE_SYS_STATFS_H */
+/* #undef HAVE_SYS_STATVFS_H */
+/* #undef HAVE_SYS_VFS_H */
+
+
+/*
+ * Location of Mac OS X localization bundle, if any.
+ */
+
+/* #undef CUPS_BUNDLEDIR */
+
+
+/*
+ * Do we have the ColorSyncRegisterDevice function?
+ */
+
+/* #undef HAVE_COLORSYNCREGISTERDEVICE */
+
+
+/*
+ * Do we have XPC?
+ */
+
+/* #undef HAVE_XPC */
+
+
+#endif /* !_CUPS_CONFIG_H_ */
+
+/*
+ * End of "$Id$".
+ */
