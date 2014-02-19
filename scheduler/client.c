1576ab1c78bcbccd496dea52e548000cf756d13d
msweet <msweet@a1ca3aef-8c08-0410-bb20-df032aa958be>
2014-02-19 20:18:10 +0000

    Mirror fixes from trunk.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.7@11623 a1ca3aef-8c08-0410-bb20-df032aa958be

--- scheduler/client.c
+++ scheduler/client.c
@@ -3708,6 +3708,14 @@ is_path_absolute(const char *path)	/* I - Input path */
     return (0);
 
  /*
+  * Check for "<" or quotes in the path and reject since this is probably
+  * someone trying to inject HTML...
+  */
+
+  if (strchr(path, '<') != NULL || strchr(path, '\"') != NULL || strchr(path, '\'') != NULL)
+    return (0);
+
+ /*
   * Check for "/.." in the path...
   */
 
