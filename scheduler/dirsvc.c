8441e27ce4008828cbdd25bdd6249c0019853b92
msweet <msweet@a1ca3aef-8c08-0410-bb20-df032aa958be>
2014-05-09 20:57:11 +0000

    CUPS did not compile when Avahi or mDNSResponder was not present (STR #4402)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@11871 a1ca3aef-8c08-0410-bb20-df032aa958be

--- scheduler/dirsvc.c
+++ scheduler/dirsvc.c
@@ -237,11 +237,13 @@ cupsdStartBrowsing(void)
   if (BrowseLocalProtocols & BROWSE_SMB)
     update_smb(1);
 
+#if defined(HAVE_DNSSD) || defined(HAVE_AVAHI)
  /*
   * Register the individual printers
   */
 
   dnssdRegisterAllPrinters(0);
+#endif /* HAVE_DNSSD || HAVE_AVAHI */
 }
 
 
@@ -255,6 +257,7 @@ cupsdStopBrowsing(void)
   if (!Browsing || !BrowseLocalProtocols)
     return;
 
+#if defined(HAVE_DNSSD) || defined(HAVE_AVAHI)
  /*
   * De-register the individual printers
   */
@@ -265,7 +268,6 @@ cupsdStopBrowsing(void)
   * Shut down browsing sockets...
   */
 
-#if defined(HAVE_DNSSD) || defined(HAVE_AVAHI)
   if ((BrowseLocalProtocols & BROWSE_DNSSD) && DNSSDMaster)
     dnssdStop();
 #endif /* HAVE_DNSSD || HAVE_AVAHI */
