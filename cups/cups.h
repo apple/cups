73f741aebc6aa24b8c1a3958ff00eb3551191639
msweet <msweet@a1ca3aef-8c08-0410-bb20-df032aa958be>
2014-06-09 18:33:24 +0000

    Really mirror fix for STR #4402 (compile problem) that wasn't picked up (STR #4424)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/branches/branch-1.7@11906 a1ca3aef-8c08-0410-bb20-df032aa958be

--- cups/cups.h
+++ cups/cups.h
@@ -53,10 +53,10 @@ extern "C" {
  * Constants...
  */
 
-#  define CUPS_VERSION			1.0703
+#  define CUPS_VERSION			1.0704
 #  define CUPS_VERSION_MAJOR		1
 #  define CUPS_VERSION_MINOR		7
-#  define CUPS_VERSION_PATCH		3
+#  define CUPS_VERSION_PATCH		4
 
 #  define CUPS_BC_FD			3
 					/* Back-channel file descriptor for
