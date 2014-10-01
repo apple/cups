92ee0d7c02e6b7a85c907c6741d1c93376b4fd44
msweet <msweet@a1ca3aef-8c08-0410-bb20-df032aa958be>
2014-10-01 19:00:21 +0000

    Save work on IPP Infra server and proxy (WIP)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@12191 a1ca3aef-8c08-0410-bb20-df032aa958be

---
+++ test/ippproxy.c
@@ -0,0 +1,29 @@
+/*
+ * "$Id$"
+ *
+ * CUPS Cloud Proxy for HP PCL and IPP Everywhere printers.
+ *
+ * Copyright 2014 by Apple Inc.
+ */
+
+#include <cups/cups.h>
+
+
+/*
+ * 'main()' - Main entry for cupsproxy.
+ */
+
+int					/* O - Exit status */
+main(int  argc,				/* I - Number of command-line arguments */
+     char *argv[])			/* I - Command-line arguments */
+{
+  (void)argc;
+  (void)argv;
+
+  return (0);
+}
+
+
+/*
+ * End of "$Id$".
+ */
