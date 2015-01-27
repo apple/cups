20529d535e6aab1d424bf115369bdc3daea009e0
msweet <msweet@a1ca3aef-8c08-0410-bb20-df032aa958be>
2015-01-27 21:22:37 +0000

    Fix copyright.
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@12429 a1ca3aef-8c08-0410-bb20-df032aa958be

--- scheduler/process.c
+++ scheduler/process.c
@@ -3,7 +3,7 @@
  *
  * Process management routines for the CUPS scheduler.
  *
- * Copyright 2007-2014 by Apple Inc.
+ * Copyright 2007-2015 by Apple Inc.
  * Copyright 1997-2007 by Easy Software Products, all rights reserved.
  *
  * These coded instructions, statements, and computer programs are the
