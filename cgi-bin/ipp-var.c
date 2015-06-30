945bef529028f571a7140b0446331cbd739028b1
msweet <msweet@a1ca3aef-8c08-0410-bb20-df032aa958be>
2015-06-30 16:13:38 +0000

    The web search incorrectly searched time-at-xxx values (STR #4652)
    git-svn-id: svn+ssh://src.apple.com/svn/cups/cups.org/trunk@12768 a1ca3aef-8c08-0410-bb20-df032aa958be

--- cgi-bin/ipp-var.c
+++ cgi-bin/ipp-var.c
@@ -222,6 +222,9 @@ cgiGetIPPObjects(ipp_t *response,	/* I - IPP response */
 	      break;
 
           case IPP_TAG_INTEGER :
+	      if (!strncmp(ippGetName(attr), "time-at-", 8))
+	        break;			/* Ignore time-at-xxx */
+
 	      for (i = 0; !add && i < attr->num_values; i ++)
 	      {
 	        char	buf[255];	/* Number buffer */
