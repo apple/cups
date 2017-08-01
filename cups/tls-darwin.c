60716f946cb4c71853e1ebf222af62c3c4c77eec
Michael Sweet <michael.r.sweet@gmail.com>
2017-08-01 00:29:21 -0400

    Update copyright.

--- cups/tls-darwin.c
+++ cups/tls-darwin.c
@@ -1,7 +1,7 @@
 /*
  * TLS support code for CUPS on macOS.
  *
- * Copyright 2007-2016 by Apple Inc.
+ * Copyright 2007-2017 by Apple Inc.
  * Copyright 1997-2007 by Easy Software Products, all rights reserved.
  *
  * These coded instructions, statements, and computer programs are the
