bea37134425f7bd0e6e1067122249705b733648a
Michael Sweet <michael.r.sweet@gmail.com>
2017-10-19 10:52:01 -0400

    Changelog and bump CUPS version.

--- vcnet/config.h
+++ vcnet/config.h
@@ -94,8 +94,8 @@ typedef unsigned long useconds_t;
  * Version of software...
  */
 
-#define CUPS_SVERSION "CUPS v2.2.4"
-#define CUPS_MINIMAL "CUPS/2.2.4"
+#define CUPS_SVERSION "CUPS v2.2.6"
+#define CUPS_MINIMAL "CUPS/2.2.6"
 
 
 /*
