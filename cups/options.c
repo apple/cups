d5bc97662f40594fddd5e48af5f47a299ac33764
Christoph Terasa <christoph@kohlio.de>
2018-04-03 21:51:46 +0200

    Remove unnecessary removal of User option in options.c

--- cups/options.c
+++ cups/options.c
@@ -79,10 +79,7 @@ cupsAddOption(const char    *name,	/* I  - Name of option */
   else if (!_cups_strcasecmp(name, "print-quality"))
     num_options = cupsRemoveOption("cupsPrintQuality", num_options, options);
   else if (!_cups_strcasecmp(name, "User"))
-  {
     cupsSetUser(value);
-    num_options = cupsRemoveOption("User", num_options, options);
-  }
 
  /*
   * Look for an existing option with the same name...
