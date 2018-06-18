de72bb61c1ee6f8e8298b664186ea84991c0ed20
Michael R Sweet <michael.r.sweet@gmail.com>
2018-06-18 14:31:23 -0400

    More tweaks for IPP Everywhere support in web interface.

--- cgi-bin/admin.c
+++ cgi-bin/admin.c
@@ -1076,9 +1076,6 @@ do_am_printer(http_t *http,		/* I - HTTP connection */
         cgiStartHTML(title);
 	if (!cgiGetVariable("PPD_MAKE"))
 	  cgiSetVariable("PPD_MAKE", cgiGetVariable("CURRENT_MAKE"));
-	if (!modify)
-	  cgiSetVariable("CURRENT_MAKE_AND_MODEL",
-	                 cgiGetArray("PPD_MAKE_AND_MODEL", 0));
         if (ipp_everywhere)
 	  cgiSetVariable("SHOW_IPP_EVERYWHERE", "1");
 	cgiCopyTemplateLang("choose-model.tmpl");
