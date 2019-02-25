88c131a16f66fafba8fad47a2466e70f9c6fc65c
Michael R Sweet <michael.r.sweet@gmail.com>
2019-02-25 15:12:29 -0500

    Fix another compiler warning.

--- ppdc/ppdc-array.cxx
+++ ppdc/ppdc-array.cxx
@@ -39,7 +39,7 @@ ppdcArray::ppdcArray(ppdcArray *a)
 
       memcpy(data, a->data, (size_t)count * sizeof(ppdcShared *));
 
-      for (int i = 0; i < count; i ++)
+      for (size_t i = 0; i < count; i ++)
         data[i]->retain();
     }
     else
