1e01995a1c5c7d406914c3aed6ceaa33f11bdf68
Michael R Sweet <michael.r.sweet@gmail.com>
2019-09-05 12:33:42 -0400

    Fix the default common name used by ippeveprinter...

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,4 @@
-CHANGES - 2.3.1 - 2019-08-27
+CHANGES - 2.3.1 - 2019-09-05
 ============================
 
 
@@ -6,6 +6,7 @@ Changes in CUPS v2.3.1
 ----------------------
 
 - Fixed spelling of "fold-accordion".
+- Fixed the default common name for TLS certificates used by `ippeveprinter`.
 
 
 Changes in CUPS v2.3.0
--- tools/ippeveprinter.c
+++ tools/ippeveprinter.c
@@ -631,10 +631,6 @@ main(int  argc,				/* I - Number of command-line args */
       _cupsLangPrintf(stderr, _("Using spool directory \"%s\"."), directory);
   }
 
-#ifdef HAVE_SSL
-  cupsSetServerCredentials(keypath, servername, 1);
-#endif /* HAVE_SSL */
-
  /*
   * Initialize DNS-SD...
   */
@@ -674,6 +670,10 @@ main(int  argc,				/* I - Number of command-line args */
   if (ppdfile)
     printer->ppdfile = strdup(ppdfile);
 #endif /* !CUPS_LITE */
+
+#ifdef HAVE_SSL
+  cupsSetServerCredentials(keypath, printer->hostname, 1);
+#endif /* HAVE_SSL */
 
  /*
   * Run the print service...
