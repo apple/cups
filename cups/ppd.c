04b4d61e593e786e3d5dc58f0628ec7b0be62591
Pranav Batra <batrapranav@chromium.org>
2020-09-29 21:50:00 +0000

    undefined behavior: signed integer overflow

--- cups/ppd.c
+++ cups/ppd.c
@@ -2839,7 +2839,7 @@ ppd_hash_option(ppd_option_t *option)	/* I - Option */
 
 
   for (hash = option->keyword[0], k = option->keyword + 1; *k;)
-    hash = 33 * hash + *k++;
+    hash = (int)(33U * (unsigned)hash) + *k++;
 
   return (hash & 511);
 }
